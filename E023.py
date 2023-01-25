225b7a4f48149a17ceaf43562f5efa96888e8bc6
reluctantgerent <108861194+reluctantgerente888@users.noreply.github.com>
2023-01-25 15:25:00 +0000

    Test replit using a forked repository.

--- E023.py
+++ E023.py
@@ -10,21 +10,21 @@
 computed_value = 0
 icheck = -1
 while icheck == -1:
-    iWidth = input("What is the width of the room in meter/s? ==> ")
-    iLength = input("What is the length of the room in meter/s? ==> ")
-    try:
-        ciWidth = float(iWidth)
-        ciLength = float(iLength)
-        icheck = 0
-    except:
-        print("Please input number data only.")
+  iWidth = input("What is the width of the room in meter/s? ==> ")
+  iLength = input("What is the length of the room in meter/s? ==> ")
+  try:
+    ciWidth = float(iWidth)
+    ciLength = float(iLength)
+    icheck = 0
+  except:
+    print("Please input number data only.")
 #--------------------------------------------------------------
 computed_value = ciWidth * ciLength
-fcomputed_value = format(computed_value,'2f')
+fcomputed_value = format(computed_value, '2f')
 final_value = str(fcomputed_value)
 #--------------------------------------------------------------
-print("The room area is",final_value,"sq. meter.")
+print("The room area is", final_value, "sq. meter.")
 print("Thank you for using this app.")
 #**************************************************************
 # Lessons Learned:
-#
+# Test
