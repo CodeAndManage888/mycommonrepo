85595c7665e14fca5a0842fba6a2bb69a4adf4ad
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-05-04 10:16:29 +0000

    Added a new script for testing but only for replit.com version. Open items for the discrepancies in computation results and another version for VS Code. E029 is also started and ready for testing.

--- bashscript/E028a_rpl.sh
+++ bashscript/E028a_rpl.sh
@@ -6,9 +6,12 @@ output_file="/home/runner/mycommonrepo/bashscript/output_data.txt"
 total_lines=$(wc -l < "$input_file")
 test_case=1
 
-for ((i = 1; i <= total_lines; i +=3)); do
-    echo "Executing Test Case ${test_case}:" | tee -a "$output_file"
-    sed -n "${i},$((i+2))p" "$input_file" | python /home/runner/mycommonrepo/E028-test.py 2>/dev/null | tee -a "$output_file"
+for ((i = 1; i <= total_lines; i += 4)); do
+    description=$(sed -n "${i}p" "$input_file")
+    echo "Executing Test Case ${test_case} ${description}:" | tee -a "$output_file"
+    
+    sed -n "$((i+1)),$((i+3))p" "$input_file" | python /home/runner/mycommonrepo/E028-test.py 2>/dev/null | tee -a "$output_file"
+    
     echo "-----------------------------------" | tee -a "$output_file"
     test_case=$((test_case + 1))
 done
