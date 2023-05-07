ed66c926d3c02eef7fc346906c9be6e3262d3527
boredmanager888 <lowenddevice888@gmail.com>
2023-05-08 00:03:07 +0800

    C05072230 - added process for sorting numbers in a 3 digit integer

--- 00E031.py
+++ 00E031.py
@@ -11,13 +11,12 @@
 #                                                             *
 #**************************************************************
 #--------------------------------------------------------------
-sample_text = "123456"
+sample_text = "612"
 num_list = []
 sum_num = 0
 
 for i in range(len(sample_text)):
     print(sample_text[i])
-#    num_list.apppend(sample_text[i])
 
 for i in range(len(sample_text)):
     sum_num = sum_num + int(sample_text[i])
@@ -27,7 +26,15 @@ print(sum_num)
 print("<---------->")
 
 for char in sample_text:
-    print(char)
+    num_list.append(char)
+print(num_list)
+
+print("<---------->")
+min_value = min(num_list)
+max_value = max(num_list)
+print(min_value, max_value)
+mid_value = str(sum_num - (int(min_value) + int(max_value)))
+print(min_value + mid_value + max_value)
 #--------------------------------------------------------------
 #**************************************************************
 # Open Items:
@@ -35,4 +42,5 @@ for char in sample_text:
 # CHistory:
 # C05072200
 # - test possible ways to break down a string
-# 
+# C05072230
+# - added process for sorting numbers in a 3 digit integer
