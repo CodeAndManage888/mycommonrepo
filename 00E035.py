259a0b4b71a4bea4a4bcb1b5415bfb7291e5476b
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-05-12 15:51:47 +0000

    C0512232350
    - updates for exercise 35

--- 00E035.py
+++ 00E035.py
@@ -1,10 +1,23 @@
 #**************************************************************
-# Date:                                                       *
-# Title:                                                      *
+# Date: 051123                                                *
+# Title: Dog Years                                            *
 # Status: In Progress (In Progress / Testing / Working)       *
+# It is commonly said that one human year is equivalent to 7  * 
+# dog years. However this simple conversion fails to          *
+# recognize that dogs reach adulthood in approximately two    * 
+# years. As a result, some people believe that it is better   *
+# to count each of the first two human years as 10.5 dog      * 
+# years, and then count each additional human year as 4 dog   * 
+# years.                                                      *
+# Write a program that implements the conversion from human   *
+# years to dog years described in the previous paragraph.     *
+# ensure that your program works correctly for conversions of *
+# less than two human years and for conversions of two or     *
+# more human years. Your program should display an            *
+# appropriate error message if the user enters a negative     *
+# number.                                                     *
 #                                                             *
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
@@ -12,5 +25,4 @@
 # Open Items:
 #
 # CHistory:
-# 
 # 
