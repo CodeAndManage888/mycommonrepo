af26e2c60a87a4b91c77c613587299a0e380af1b
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-05-27 02:28:05 +0000

    C0527231015
    -  finalize the requirement for exercise 44
    - rename and transfer exercise 43

--- 00E044.py
+++ 00E044.py
@@ -1,10 +1,31 @@
 #**************************************************************
-# Date:                                                       *
-# Title:                                                      *
+# Date: 052623   (Expected Solution with 18 Lines of Code)    *
+# Title: Date to Holiday Name                                 *
 # Status: In Progress (In Progress / Testing / Working)       *
+# Canada has three national holidays which fall on the same   *
+# dates each year. Write a program that reads a month and day *
+# from the user. If the month and day match one of the        *
+# holidays listed previously then your proogram should        *
+# display the holiday's name. Otherwise your program should   *
+# indicate that the entered month and day do not correspond   *
+# to the fixed-date holiday.                                  *
+# Canada has two additional national holidays, Good Friday    *
+# and Labour Day, whose dates vary from year to year. There   *
+# are also numerous provincial and territorial holidays, some *
+# of which have fixed dates, and some of which have variable  *
+# dates. We will not consider any of these additional         *
+# holidays in this exercise.                                  *
 #                                                             *
+# Coder Additional Features:                                  *
+# 1.) The program will be able to accept 1 format of the date *
+#     but can distinguesh if it is written in upper or lower  *
+#     case.                                                   *
+# 2.) The program will only accept 1 input e.g. Jan 1 but no  *
+#     year will be accepted.                                  *
+# 3.) That program will be able to detect if the data is      *
+#     valid or not e.g. Feb 30                                *
+
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
@@ -12,5 +33,5 @@
 # Open Items:
 #
 # CHistory:
-# 
-# 
+# C0527231015
+# - Finalize the requirement for exercise 44.
