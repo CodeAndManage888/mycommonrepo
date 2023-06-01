32dc750bca19f9f463673b33d1ef9f2cc4037a18
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-06-01 11:03:21 +0000

    C0601231700
    - finalize requirement for exercise 51 and 52

--- 00E051.py
+++ 00E051.py
@@ -1,10 +1,32 @@
 #**************************************************************
-# Date:                                                       *
-# Title:                                                      *
+# Date: 060123   (Expected Solution with 52 Lines of Code)    *
+# Title: Letter Grade to Grade Points                         *
 # Status: In Progress (In Progress / Testing / Working)       *
+# At a particular university, letter grades are mapped to     *
+# grade points in the following manner:                       *
+#                                                             *
+# Letter                  Grade Points                        *
+# --------                ---------------                     *
+# A+                      4.0                                 *
+# A                       4.0                                 *
+# A-                      3.7                                 *
+# B+                      3.3                                 *
+# B                       3.0                                 *
+# B-                      2.7                                 *
+# C+                      2.3                                 *
+# C                       2.0                                 *
+# C-                      1.7                                 *
+# D+                      1.3                                 *
+# D                       1.0                                 *
+# A+                      0.0                                 *
+#                                                             *
+# Write a program that begins by reading a letter grade from  *
+# the user. Then your program should compute and display the  *
+# equivalent number of grade points. Ensure that your program *
+# generates an appropriate error message if the user enters   *
+# an invalid letter grade.                                    *
 #                                                             *
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
@@ -12,5 +34,5 @@
 # Open Items:
 #
 # CHistory:
-# 
-# 
+# C0601231700
+# - finalize requirement for exercise 51 and 52
--- 00E052.py
+++ 00E052.py
@@ -1,10 +1,18 @@
 #**************************************************************
-# Date:                                                       *
-# Title:                                                      *
+# Date: 060123   (Expected Solution with 47 Lines of Code)    *
+# Title: Grade Points to Letter Grade                         *
 # Status: In Progress (In Progress / Testing / Working)       *
+# In the previous exercise you created a program that         *
+# converts a ltter grade into the equivalent number of grade  *
+# points. In this exercise you will create a program that     *
+# reverses the process and converts from a grade point value  *
+# entered by the user to a letter grade. Ensure that your     *
+# program handles grade point values that fall between letter *
+# grades. These should be rounded to the closest letter       *
+# grade. Your program should report A+ for a 4.0 (or greater) *
+# grade point average.                                        *
 #                                                             *
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
@@ -12,5 +20,5 @@
 # Open Items:
 #
 # CHistory:
-# 
-# 
+# C0601231700
+# - finalize requirement for exercise 51 and 52
