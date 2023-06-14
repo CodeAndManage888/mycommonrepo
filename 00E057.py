c18beca3f9550d47bd6f4de2ad324fc5e203a58e
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-06-14 11:56:25 +0000

    C0614231930
    - finalize requirement for exercise 57 an 58

--- 00E057.py
+++ 00E057.py
@@ -1,10 +1,26 @@
 #**************************************************************
-# Date:                                                       *
-# Title:                                                      *
+# Date: 061423   (Expected Solution with 22 Lines of Code)    *
+# Title: Is it a Leap Year                                    *
 # Status: In Progress (In Progress / Testing / Working)       *
+# Most years have 365 days. However, the time required for    *
+# the Earth to orbit the Sun is actually slightly more than   *
+# that. As a result, an extra day, February 29, is included   *
+# in some years to correct for this difference. Such years    *
+# are referred to as leap years. The rules for determiniing   *
+# whether or not a year is a leap year follow:                *
+#                                                             *
+# a.) Any year that is divisible by 400 is a leap year.       *
+# b.) Of the remaining years, any year that is divisible by   *
+#     100 is not a leap year.                                 *
+# c.) Of the remaining years, any year that is divisible by 4 *
+#     is a leap year.                                         *
+# d.) All other year are not leap years.                      *
+#                                                             *
+# Write a program that reads a year from the user and         *
+# displays a message indicating whether or not it is a leap   *
+# year.                                                       *
 #                                                             *
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
