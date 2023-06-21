2bca202bdb476876ebdf821517eeabfd5699ea8d
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-06-21 13:37:39 +0000

    C0621232130
    - continue coding excercise 54

--- 00E054.py
+++ 00E054.py
@@ -24,4 +24,44 @@
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
+iMagValue, ciMagValue = (" ", 0)
+icheck = -1
+#--------------------------------------------------------------
+def data_check(UserIn1):
+  global icheck
+  try:
+    cUserIn1=int(UserIn1)
+    icheck = 0
+    return cUserIn1
+  except:
+    print("Invalid input data! Integer input data only.")
+    cUserIn1 = 0
+    return cUserIn1
+#--------------------------------------------------------------
+iMagValue = input("Enter the earthquake magnitude(Numeric only): ")
+ciMagValue = data_check(iMagValue)
+if icheck == 0:
+  if 0 < ciMagValue < 2.0:
+    print("The value %s in the Richter Scale is Micro" % ciMagValue)
+  elif 2.0 <= ciMagValue < 3.0:
+    print("The value %s in the Richter Scale is Very Minor" % ciMagValue)
+  elif 3.0 <= ciMagValue < 4.0:
+    print("The value %s in the Richter Scale is Minor" % ciMagValue)
+  elif 4.0 <= ciMagValue < 5.0:
+    print("The value %s in the Richter Scale is Light" % ciMagValue)
+  elif 5.0 <= ciMagValue < 6.0: 
+    print("The value %s in the Richter Scale is Moderate" % ciMagValue)
+  elif 6.0 <= ciMagValue < 7.0:
+    print("The value %s in the Richter Scale is Strong" % ciMagValue)
+  elif 7.0 <= ciMagValue < 8.0:
+    print("The value %s in the Richter Scale is Major" % ciMagValue)
+  elif 8.0 <= ciMagValue < 10.0:
+    print("The value %s in the Richter Scale is Great" % ciMagValue)
+  elif ciMagValue >= 10.0:
+    print("The value %s in the Richter Scale is Meteoric" % ciMagValue)
+  else:
+    print("Invalid Data! Out of range data")
+  print("Thank you for using this app.")
+else:
+  print("Thank you for using this app.")
 #**************************************************************
