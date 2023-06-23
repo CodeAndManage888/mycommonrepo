fbfa3c59a458df398a180c8758917c5cd33366fc
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-06-23 14:15:42 +0000

    C0623232200
    - continue coding exercise 55

--- 00E055.py
+++ 00E055.py
@@ -21,6 +21,43 @@
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
+import math
 #--------------------------------------------------------------
+iFreqValue, ciFreqValue, fin_val_num = (" ", 0, 0)
+icheck = -1
 #--------------------------------------------------------------
+def data_check(UserIn1):
+  global icheck
+  try:
+    cUserIn1=int(UserIn1)
+    icheck = 0
+    return cUserIn1
+  except:
+    print("Invalid input data! Integer input data only.")
+    cUserIn1 = 0
+    return cUserIn1
+#--------------------------------------------------------------
+iFreqValue = input("Enter the Frequency value in Hz(Numeric only): ")
+ciFreqValue = data_check(iFreqValue)
+fin_val_num = "{:.0e}".format(ciFreqValue)
+if icheck == 0:
+  if ciFreqValue < 3e9:
+    print("The Frequency %s Hz is within the range of a Radio waves." % fin_val_num)
+  elif 3e9 <= ciFreqValue < 3e12: #3 x 10^9 to less than 3 x 10^12
+    print("The Frequency %s Hz is within the range of a Microwaves." % fin_val_num)
+  elif 3e12 <= ciFreqValue < 4.3e14: #3 x 10^12 to less than 4.3 x 10^14
+    print("The Frequency %s Hz is within the range of an Infrared light." % fin_val_num)
+  elif 4.3e14 <= ciFreqValue < 7.5e14: #4.3 x 10^14 to less than 7.5 x 10^14
+    print("The Frequency %s Hz is within the range of a Visible light." % fin_val_num)
+  elif 7.5e14 <= ciFreqValue < 3e17: #7.5 x 10^14 to less than 3 x 10^17
+    print("The Frequency %s Hz is within the range of an Ultraviolet light." % fin_val_num)
+  elif 3e17 <= ciFreqValue <= 3e19: #3 x 10^17 to less than 3 x 10^19
+    print("The Frequency %s Hz is within the range of an X-rays." % fin_val_num)
+  elif 3e19 <= ciFreqValue: #3 x 10^19 or more
+    print("The Frequency %s Hz is within the range of a Gamma rays." % fin_val_num)
+  else:
+    print("Invalid Data! Out of range data")
+  print("Thank you for using this app.")
+else:
+  print("Thank you for using this app.")
 #**************************************************************
--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 #**************************************************************
-# Date: 060923   (Expected Solution with 38 Lines of Code)    *
-# Title: Wavelengths of Visible Light                         *
-# Status: Testing (In Progress / Testing / Working)           *
-# The wavelength of visible light ranges from 380 to 750      *
-# nanometers (nm). While the spectrum is continuous, it is    *
-# often divided into 6 colors as shown below:                 *
+# Date: 060923   (Expected Solution with 31 Lines of Code)    *
+# Title: Frequency to Name                                    *
+# Status: In Progress (In Progress / Testing / Working)       *
+# Electromagnetic radiation can be classified into one of 7   *
+# categories according to its frequency, as shown in the      *
+# table below:                                                *
 #                                                             *
-# Color               Wavelength (nm)                         *
-# ----------          ----------------------                  *
-# Violet              380 to less than 450                    *
-# Blue                450 to less than 495                    *
-# Green               495 to less than 570                    *
-# Yellow              570 to less than 590                    *
-# Orange              590 to less than 620                    *
-# Red                 620 to 750                              *
+# Name                  Frequency Range (Hz)                  *
+# ---------------       --------------------------------      *
+# Radio waves           Less than 3 x 10^9                    *
+# Microwaves            3 x 10^9 to less than 3 x 10^12       *
+# Infrared light        3 x 10^12 to less than 4.3 x 10^14    *
+# Visible light         4.3 x 10^14 to less than 7.5 x 10^14  *
+# Ultraviolet light     7.5 x 10^14 to less than 3 x 10^17    *
+# X-rays                3 x 10^17 to less than 3 x 10^19      *
+# Gamma rays            3 x 10^19 or more                     *
 #                                                             *
-# Write a program that reads a wavelength from the user and   *
-# reports its color. Display an appropriate error message if  *
-# the wavelength entered by the user is outside of the        *
-# visible spectrum.                                           *
+# Write a program that reads the frequency of the radiation   *
+# from the usr and displays the appropriate name.             *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
-iWaveLenValue, ciWaveLenValue = (" ", 0)
+import math
+#--------------------------------------------------------------
+iFreqValue, ciFreqValue, fin_val_num = (" ", 0, 0)
 icheck = -1
 #--------------------------------------------------------------
 def data_check(UserIn1):
@@ -36,21 +37,24 @@ def data_check(UserIn1):
     cUserIn1 = 0
     return cUserIn1
 #--------------------------------------------------------------
-iWaveLenValue = input("Enter the Wavelength value(Numeric only): ")
-ciWaveLenValue = data_check(iWaveLenValue)
+iFreqValue = input("Enter the Frequency value in Hz(Numeric only): ")
+ciFreqValue = data_check(iFreqValue)
+fin_val_num = "{:.0e}".format(ciFreqValue)
 if icheck == 0:
-  if 280 <= ciWaveLenValue < 450:
-    print("The Wavelength %snm is Color Violet." % ciWaveLenValue)
-  elif 450 <= ciWaveLenValue < 495:
-    print("The Wavelength %s nm is Color Blue." % ciWaveLenValue)
-  elif 495 <= ciWaveLenValue < 570:
-    print("The Wavelength %s nm is Color Green." % ciWaveLenValue)
-  elif 570 <= ciWaveLenValue < 590:
-    print("The Wavelength %s nm is Color Yellow." % ciWaveLenValue)
-  elif 590 <= ciWaveLenValue < 620: 
-    print("The Wavelength %s nm is Color Orange." % ciWaveLenValue)
-  elif 620 <= ciWaveLenValue <= 750:
-    print("The Wavelength %s nm is Color Red." % ciWaveLenValue)
+  if ciFreqValue < 3e9:
+    print("The Frequency %s Hz is within the range of a Radio waves." % fin_val_num)
+  elif 3e9 <= ciFreqValue < 3e12: #3 x 10^9 to less than 3 x 10^12
+    print("The Frequency %s Hz is within the range of a Microwaves." % fin_val_num)
+  elif 3e12 <= ciFreqValue < 4.3e14: #3 x 10^12 to less than 4.3 x 10^14
+    print("The Frequency %s Hz is within the range of an Infrared light." % fin_val_num)
+  elif 4.3e14 <= ciFreqValue < 7.5e14: #4.3 x 10^14 to less than 7.5 x 10^14
+    print("The Frequency %s Hz is within the range of a Visible light." % fin_val_num)
+  elif 7.5e14 <= ciFreqValue < 3e17: #7.5 x 10^14 to less than 3 x 10^17
+    print("The Frequency %s Hz is within the range of an Ultraviolet light." % fin_val_num)
+  elif 3e17 <= ciFreqValue <= 3e19: #3 x 10^17 to less than 3 x 10^19
+    print("The Frequency %s Hz is within the range of an X-rays." % fin_val_num)
+  elif 3e19 <= ciFreqValue: #3 x 10^19 or more
+    print("The Frequency %s Hz is within the range of a Gamma rays." % fin_val_num)
   else:
     print("Invalid Data! Out of range data")
   print("Thank you for using this app.")
