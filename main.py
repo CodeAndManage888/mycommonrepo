d30db1e7f657af72d7e428f3572854e6209ecf6b
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-07-02 08:37:51 +0000

    C0702231630
    - completed coding for exercise 61 and now ready for testing.

--- main.py
+++ main.py
@@ -1,91 +1,43 @@
+#!/bin/bash
 #**************************************************************
-# Date: 061523   (Expected Solution with 45 Lines of Code)    *
-# Title: Roulette Payouts                                     *
+# Date: 063023   (Expected Solution with 26 Lines of Code)    *
+# Title: Average                                              *
 # Status: In Progress (In Progress / Testing / Working)       *
-# A roulette wheel has 38 spaces on it. Of these spaces, 18   *
-# are black, 18 are red, and two are green. The green spaces  *
-# are numbered 0 and 00. The red spaces are numbered 1, 3, 5, *
-# 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34 and    *
-# 36. The remaining integers between 1 and 36 are used to     *
-# number the black spaces.                                    *
-# Many different bets can be placed in roulette. We will only *
-# consider the following subset of them in this exercise:     *
+# In this exercise you will create a program that computes    *
+# the average of a collection of values entered by the user.  *
+# The user will enter 0 as a sentinel value to indicate that  *
+# no further values will be provided. Your program should     *
+# display an appropriate error message if the first value     *
+# entered by the user is 0.                                   *
 #                                                             *
-# a. Single number (1 to 36, 0, or 00)                        *
-# b. Red versus Black                                         *
-# c. Odd versus Even (Note that 0 and 00 do not pay out for   *
-# even)                                                       *
-# d. 1 to 18 versus 19 to 36                                  *
+# Hint: Because the 0 marks the end of the input it should    *
+# not be included in the average.                             *
 #                                                             *
-# Write a program that simulates a spin of a roulette wheel   *
-# by using Python's random number generator. Display the      *
-# number that was selected and all of the bets that must be   *
-# payed. For example, if 13 is selected then your program     *
-# should display:                                             *
-#                                                             *
-# The spin resulted in 13 . . .                               *
-# Pay 13                                                      *
-# Pay Black                                                   *
-# Pay Odd                                                     *
-# Pay 1 to 18                                                 *
-#                                                             *
-# If the simulation results in 0 or 00 then your program      *
-# should display Pay 0 or Pay 00 without any further output.  *
 # Computed Result Validated:                                  *
 #**************************************************************
-import random
-import time
-status_bar = ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-green_num_list = ["0", "00"]
-red_num_list = ["1", "3", "5", "7", "9", "12", "14", "16", "18",
-                "19", "21", "23", "25", "27", "30", "32", "34", 
-                "36"]
-black_num_list = ["2", "4", "6", "8", "10", "11", "13", "15", 
-                  "17", "20", "22", "24", "26", "28", "29", "31",
-                  "33", "35"]
-odd_num_list = ["0", "00", "1", "3", "5", "7", "9", "11", "13", 
-                "15", "17", "19", "21", "23", "25", "27", "29", 
-                "31", "33", "35"]
-even_num_list = ["2", "4", "6", "8", "10", "12", "14", "16", 
-                 "18", "20", "22", "24", "26", "28", "30", "32", 
-                 "34", "36"]
-first_half_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9", 
-                   "10", "11", "12", "13", "14", "15", "16", "17",
-                   "18"]
-second_half_list = ["19", "20", "21", "22", "23", "24", "25", 
-                    "26", "27", "28", "29", "30", "31", "32", "33", 
-                    "34", "35", "36"]
-sing_num_list = ["0", "00", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
-                 "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-                 "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
-                 "31", "32", "33", "34", "35", "36"]
 #--------------------------------------------------------------
-print("Roulette Wheel is now closed for bets!")
-print("Roulette Wheel is spining: ", end = "")
-#for character in status_bar:
-#  print(character, end = "")
-#  time.sleep(1)
-for _ in range(20):  # this will print ">" 20 times
-    print(">", end="")
-    time.sleep(1)  # delay of 1 second
-print("\n")
-selected_number = random.choice(sing_num_list)
-print("The spin resulted in %s" % selected_number)
-print("Please pay the following: ")
-print("a.) Pay bets for %s" % selected_number)
-if selected_number in green_num_list:
-  print("b.) Pay bets for 0 or 00")
-if selected_number in red_num_list:
-  print("b.) Pay bets for red")
-if selected_number in black_num_list:
-  print("b.) Pay bets for black")
-if selected_number in odd_num_list:
-  print("c.) Pay bets for odd")
-if selected_number in even_num_list:
-  print("c.) Pay bets for even")
-if selected_number in first_half_list:
-  print("d.) Pay bets for 1 to 18")
-if selected_number in second_half_list:
-  print ("d.) Pay bets for 19 to 36")
+Processed_List = [" "]
+CollectCheck, Value_Counter, Total_Value, Ave_Value = (0, 0, 0, 0.0)
+#--------------------------------------------------------------
+def data_check(UserIn1):
+  global icheck
+  try:
+    cUserIn1 = int(UserIn1)
+    icheck = 0
+    return cUserIn1
+  except:
+    print("Invalid input data! Numeric input data only.")
+#-------------------------------------------------------------- 
+iCollectValues = input("Please enter set of numeric value: ")
+CollectCheck = data_check(iCollectValues.replace(" ",""))
+if icheck == 0:
+  Processed_List = iCollectValues.split()
+  for values in Processed_List:
+    if values != "0":
+      Value_Counter = Value_Counter + 1
+      Total_Value = Total_Value + int(values)
+    else:
+      Ave_Value = Total_Value / Value_Counter
+      print("The average of the collection is %s." % Ave_Value)
 print("Thank you for using this app.")
 #**************************************************************
