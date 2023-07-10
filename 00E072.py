f52fd0158c4b2c9b04035a51b1f626436d14738c
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-07-10 15:07:56 +0000

    C0710232300
    - completed requirement for exercise 71 to 73

--- 00E072.py
+++ 00E072.py
@@ -1,11 +1,16 @@
 #!/bin/bash
 #**************************************************************
-# Date:                                                       *
-# Title:                                                      *
+# Date: 071023   (Expected Solution with 23 Lines of Code)    *
+# Title: Is a String a Palindrome                             *
 # Status: In Progress (In Progress / Testing / Working)       *
+# A string is a palindrome if it is identical forward and     *
+# backward. For example "anna", "civic", "level" and "hannah" *
+# are all examples of palindromic words. Write a program that *
+# reads a string from the user and uses a loop to determines  *
+# whether or not it is a palindrome. Display the result,      *
+# including a meaningful output message.                      *
 #                                                             *
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 #--------------------------------------------------------------
