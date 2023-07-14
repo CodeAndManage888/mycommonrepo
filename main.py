97cf730cba4210185cc4f4fe7ad1a07d23e1f65f
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-07-14 02:05:15 +0000

    C0714231100
    - continue coding exercise 67

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
 #!/bin/bash
 #**************************************************************
-# Date: 070823   (Expected Solution with 62 Lines of Code)    *
-# Title: Compute a Grade Point Average                        *
-# Status: Testing (In Progress / Testing / Working)           *
-# Exercise 51 included a table that shows the conversion from *
-# letter grades to gradepoints at a particular academic       *
-# institution. In this exercise you will compute the grade    *
-# point average of an arbitrary number of letter grades       *
-# entered by the user. The user will enter a blank line to    *
-# indicate that all of the grades have been provided. For     *
-# example, if the user enters A, followed by C+, followed by  *
-# B, followed by a blank line then your program should report *
-# a grade point average of 3.1.                               *
-# You may find your solution to Exercise 51 helpful when      *
-# completing this exercise.Your program does not need to do   *
-# any error checking. It can assume that each value entered   *
-# by the user will always be a valid letter grade or a blank  *
-# line.                                                       *
+# Date: 070823   (Expected Solution with 38 Lines of Code)    *
+# Title: Admission Price                                      *
+# Status: In Progress (In Progress / Testing / Working)       *
+# A particular zoo determines the price of admission based on *
+# the age of the guest. Guests 2 years of age and less are    *
+# admitted without charge. Children between 3 and 12 years of *
+# age cost $14.00. Seniors aged 65 years and over cost $18.00.*
+# Admission for all other guests is $23.00.                   *
+# Create a program that begins by reading the ages of all of  *
+# the guests in a group from the user, with one age entered   *
+# on each line. The user will enter a blank line to indicate  *
+# that there are no more guests in the group. Then your       *
+# program should display the admission cost for the group     *
+# with an appropriate message. The cost should be displayed   *
+# using two decimal places.                                   *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
-LetterGradeDict = {"A+": 4.0, "A": 4.0, "A-": 3.7, "B+": 3.3,
-                     "B": 3.0, "B-": 2.7, "C+": 2.3, "C": 2.0,
-                     "C-": 1.7, "D+": 1.3, "D": 1.0, "F": 0.0}
-val_let_grade_list=["A+", "a+", "A", "a", "A-", "a-", "B+", "b+", "B", "b", "B-",
-            "b-", "C+", "c+", "C", "c", "C-", "c-", "D+", "d+", "D", "d","F","f"," "]
-iLetterGrade, ciLetterGrade, SumGradePt, GradeCtr, ErrorFlag = ("", " ", 0.0, 0.0, 0)
+icheck, GuestAge, cGuestAge, TotalCharge = (0, "", 0, 0.00)
 #--------------------------------------------------------------
-while iLetterGrade != " ":
-  iLetterGrade = input("Enter the letter grade(A+, A etc)? ==> ")
-  if iLetterGrade in val_let_grade_list:
-    ciLetterGrade = iLetterGrade.upper()
-    if iLetterGrade != " ":
-      SumGradePt = SumGradePt + LetterGradeDict[ciLetterGrade]
-      GradeCtr = GradeCtr + 1
+def data_check(UserIn1):
+  global icheck
+  try:
+    cUserIn1 = int(UserIn1)
+    icheck = 0
+    return cUserIn1
+  except:
+    icheck = -1
+    print("Invalid input data! Numeric input data only.")
+#--------------------------------------------------------------
+while GuestAge != " ":
+  GuestAge = input("Input the age of the guess: ")
+  if GuestAge != " ":
+    cGuestAge = data_check(GuestAge)
+    if icheck == -1:
+      break
+  if cGuestAge <= 2:
+    TotalCharge = TotalCharge + 0.00
+  elif cGuestAge >= 3 and cGuestAge <= 12:
+    TotalCharge = TotalCharge + 14.00
+  elif cGuestAge >= 65:
+    TotalCharge = TotalCharge + 18.00
   else:
-    print("The %s letter grade is invalid." % iLetterGrade)
-    ErrorFlag = 1
-    break
-if ErrorFlag == 0:
-  GradePtAve = SumGradePt / GradeCtr
-  print("The average of these letter grades is %s grade point(s)." % GradePtAve)
+    TotalCharge = TotalCharge + 23.00
+  print("Total admission cost is %s" % format(TotalCharge, '0.2f'))
 print("Thank you for using this app.")
 #**************************************************************
