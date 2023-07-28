145e4d830f087a94bb370ec9783e6ee70976c670
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-07-28 15:30:08 +0000

    C0728232330
    - completed coding for exercise 76

--- main.py
+++ main.py
@@ -1,31 +1,18 @@
 #!/bin/bash
 #**************************************************************
-# Date: 072623   (Expected Solution with 27 Lines of Code)    *
-# Title: Prime Factors                                        *
+# Date: 072623   (Expected Solution with 18 Lines of Code)    *
+# Title: Binary to Decimal                                    *
 # Status: In Progress (In Progress / Testing / Working)       *
-# The prime factorization of an integer, n, can be determined *
-# using the following steps:                                  *
-#                                                             *
-# Initialize factor to two                                    *
-# While factor is less than or equal to n do                  *
-#   If n is evenly divisible by factor then                   *
-#      Conclude that factor is a factor of n                  *
-#      Divide n by factor using integer division              *
-#   Else                                                      *
-#      Increase factor by one                                 *
-# Write a program that reads an integer from the user. If the *
-# value entered by the user is less than 2 then your program  *
-# should display an appropriate error message. Otherwise your *
-# program should display the prime numbers that can be        *
-# multiplied together to compute n, with one factor appearin  *
-# on each line. For example:                                  *
-#                                                             *
-# Enter an integer (2 or greater): 72                         *
-# The prime factors of 72 are: 2 2 2 3 3                      *
+# Write a program that converts a binary (base 2) number to   *
+# decimal (base 10). Your program should begin by reading the *
+# binary number from the user as a string. Then it should     *
+# compute the quivalent decimal number by processing each     *
+# digit in the binary number. Finally, your program should    *
+# display the equivalent decimal number with an appropriate   *
+# message.                                                    *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
-prime_factor_list = []
 #--------------------------------------------------------------
 def data_check(UserIn1):
   global icheck
@@ -37,34 +24,5 @@ def data_check(UserIn1):
     icheck = -1
     print("Invalid input data! Numeric input data only.")
 #--------------------------------------------------------------
-UserNum = input("Enter the input number: ")
-cUserNum = data_check(UserNum)
-factor = 2
-while factor <= cUserNum:
-  remainder = cUserNum % factor
-  if remainder == 0:
-    prime_factor_list.append(factor)
-    cUserNum = cUserNum // factor
-  else:
-    factor += 1
-print("The prime factors of %s are: " % UserNum, end="")
-#print(len(prime_factor_list))
-for prime in prime_factor_list:
-  if prime == prime_factor_list[-1]:
-    print(prime," ")
-  else:
-    print(prime," ", end="")  
-#while (len(prime_factor_list) - ctr) != 1:
-#  if 
-#  print(prime," ", end="")
-#  if (len(prime_factor_list) - prime_factor_list.index(prime)) != 1:
-#    print("1st")
-#    print((len(prime_factor_list) - prime_factor_list.index(prime)))
-#    print(prime," ", end="")
-#  else:
-#    print("2nd")
-#    print((len(prime_factor_list) - prime_factor_list.index(prime)))
-#    print(prime," ", end="")
-#print("Thank you for using this app.", end="\n")
 print("Thank you for using this app.")
 #**************************************************************
