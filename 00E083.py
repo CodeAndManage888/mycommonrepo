4349b4a107692a70284a2b1a9336fd330e0554e4
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-08-02 03:05:03 +0000

    C0802231100
    - added templates for the new set of exercises and finalize the requirement of 81 to 85
    - started coding for exercise 81

--- 00E083.py
+++ 00E083.py
@@ -1,11 +1,17 @@
 #!/bin/bash
 #**************************************************************
-# Date: MMDDYY   (Expected Solution with XX Lines of Code)    *
-# Title:                                                      *
+# Date: 080123   (Expected Solution with 23 Lines of Code)    *
+# Title: Shipping Calculator                                  *
 # Status: In Progress (In Progress / Testing / Working)       *
+# An online retailer provides express shipping for many of    *
+# its items at a rate of $10.95 for the first item, and $2.95 *
+# for each subsequent item. Write a function that takes the   *
+# number of items in the order as its only parameter. Return  *
+# the shipping charge for the order as the function's result. *
+# Include a main program that reads the number of items       *
+# purchased from the user and displays the shipping charge.   *
 #                                                             *
 # Computed Result Validated:                                  *
-#                                                             *
 #**************************************************************
 #--------------------------------------------------------------
 def data_check(UserIn1):
