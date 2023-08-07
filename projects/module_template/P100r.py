8b9ba158ea2da56b44d7245ae7ac21b5aaa0dc46
Frederick Erick Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2023-08-07 04:45:10 +0000

    C0807231300 - completed coding for the basic feature of P100 project - sample output file are also uploaded - the following will be logged as an issue/additional feature:   a. handling of 2 page detail and exercise that exactly ends at the      end of the page   b. detail of the exercise that are considered example must not be      broken down into a words. example exerice are 86, 80, 76 etc.

--- projects/module_template/P100r.py
+++ projects/module_template/P100r.py
@@ -10,20 +10,20 @@ def read_pdf(file_path, PageExerNoStr):
         pdf_reader = PyPDF2.PdfReader(file)
 
         # Get the total number of pages in the PDF
-        num_pages = len(pdf_reader.pages)                                 # Do I still need this?
+        num_pages = len(pdf_reader.pages)                                                # Do I still need this?
 
         # Get the text data
         page = pdf_reader.pages[int(Target_List[0])]
         text = page.extract_text()      
-#        print(text)                                                       # Uncomment for debugging
+#        print(text)                                                                     # Uncomment for debugging
 
         # Prepare the extraction of the target exercise only.
         content = text.split("\n")
-#        print(content)                                                    # Uncomment for debugging
+#        print(content)                                                                  # Uncomment for debugging
         start_txt = "Exercise " + Target_List[1] + ":"
         end_txt = "Exercise " + str(int(Target_List[1]) + 1) + ":"
-#        print(start_txt)                                                  # Uncomment for debugging
-#        print(end_txt)                                                    # Uncomment for debugging
+#        print(start_txt)                                                                # Uncomment for debugging
+#        print(end_txt)                                                                  # Uncomment for debugging
         return_block = []
         start_pos_list = []
         end_pos_list = []
@@ -38,8 +38,8 @@ def read_pdf(file_path, PageExerNoStr):
                 end_pos_list.append(idx1)
                 break
         
-#        print(start_pos_list)                                             # Uncomment for debugging
-#        print(end_pos_list)                                               # Uncomment for debugging
+#        print(start_pos_list)                                                           # Uncomment for debugging
+#        print(end_pos_list)                                                             # Uncomment for debugging
 
         line_ctr = start_pos_list[0]
         end_ctr = len(end_pos_list) - 1
@@ -55,19 +55,19 @@ def read_pdf(file_path, PageExerNoStr):
                 return_block.append(content[line_ctr + add_pos])
             line_ctr += 1
 
-#        print(return_block)                                              # Uncomment for debugging
+#        print(return_block)                                                             # Uncomment for debugging
         return return_block
 
 def format_then_write(textdata):
-    file_path = "/home/runner/mycommonrepo/projects/module_template/outfile/temp000.py"
+    file_path = "/workspaces/mycommonrepo/projects/module_template/outfile_cloud/gen_template.py"
     # The following code will format the text
     current_date = datetime.datetime.now()
-    formatted_date = current_date.strftime("%m%d%y")                     # Current date format MMDDYY
+    formatted_date = current_date.strftime("%m%d%y")                                     # Current date format MMDDYY
     final_recs = []
     line_code = textdata[1]
     TotCodPos = line_code.find("—")
-#    print(textdata)                                                     # Uncomment for debugging
-#    print(line_code)                                                    # Uncomment for debugging
+#    print(textdata)                                                                     # Uncomment for debugging
+#    print(line_code)                                                                    # Uncomment for debugging
     rec00 = "#!/bin/bash"
     final_recs.append(rec00)
     rec01 = "#**************************************************************"
@@ -83,22 +83,32 @@ def format_then_write(textdata):
     final_recs.append(rec04)
 
     word_list = []
-    word_ctr, item_ctr, reqtdetail = (0, 2, "")
-    while item_ctr <= (len(textdata) - 2):
-        word_list += textdata[item_ctr].split(" ")
-        item_ctr += 1
-    print(word_list)
-    print(len(word_list))
+    for idx3, readline in enumerate(textdata):
+        if idx3 != 0 and idx3 != 1:
+            word_list += readline.split(" ")
 
-    while word_ctr <= len(word_list):
-        if len(reqtdetail) < 59:
-            popped_word = word_list.pop(word_ctr)
-            reqtdetail += popped_word + " "
-            if (len(reqtdetail) + len(word_list[word_ctr + 1])) > 59:
-                rec05 = "# " + reqtdetail + "  *"
+#    print(word_list)                                                                    # Uncomment for debugging
+#    print(len(word_list))                                                               # Uncomment for debugging
+
+    reqtdetail = ""
+    for idx2, word in enumerate(word_list):
+        reqtdetail += word + " "
+        if idx2 != (len(word_list) - 1):
+            if (len(reqtdetail) + len(word_list[idx2 + 1])) > 59:
+                addspaces = 59 - (len(reqtdetail) - 1)
+                rec05 = "# " + reqtdetail + (addspaces * " ") + "*"
                 final_recs.append(rec05)
-        word_ctr += 1
-    
+                reqtdetail = ""
+        else:
+            addspaces = 59 - (len(reqtdetail) - 1)
+            rec05 = "# " + reqtdetail + (addspaces * " ") + "*"
+            final_recs.append(rec05)
+            reqtdetail = ""
+
+#    reqtdetail = "                                                          "           # Uncomment for debugging
+#    rec05 = "# " + reqtdetail + "  *"                                                   # Uncomment for debugging
+#    final_recs.append(rec05)                                                            # Uncomment for debugging
+
     rec06 = "#                                                             *"
     final_recs.append(rec06)
     rec07 = "# Computed Result Validated:                                  *"
@@ -106,12 +116,15 @@ def format_then_write(textdata):
     rec08 = "#**************************************************************"
     final_recs.append(rec08)
 
-    for item in final_recs:                                               # Uncomment for debugging
-        print(item)                                                       # Uncomment for debugging
+    with open(file_path, "w") as file_handle:
+        for item in final_recs:
+            print(item)                                                                  # Uncomment for debugging
+            print(item, file=file_handle)
+    file_handle.close()
     
 if __name__ == "__main__":
     UserPageIn = input("Enter the target page(s) & exercise number: ")
-#   pdf_file_path = "/home/runner/mycommonrepo/projects/module_template/infile_replit/The Python Workbook.pdf"           # Path to your PDF file (Cloud)
-    pdf_file_path = "/workspaces/mycommonrepo/projects/module_template/infile_replit/The Python Workbook.pdf"            # Path to your PDF file (Cloud)
+#   pdf_file_path = "/home/runner/mycommonrepo/projects/module_template/infile_replit/The Python Workbook.pdf"          # Path to your PDF file (Cloud)
+    pdf_file_path = "/workspaces/mycommonrepo/projects/module_template/infile_cloud/The Python Workbook.pdf"            # Path to your PDF file (Cloud)
     tmp_data = read_pdf(pdf_file_path, UserPageIn)
     format_then_write(tmp_data)
