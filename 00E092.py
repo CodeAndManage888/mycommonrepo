60f3fa022e3dfe44c3a20ebf3c54871d0612fd72
Frederick Erick Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2023-08-15 11:04:34 +0000

    C0815231900 - added 5 more template for exercie 91 to 95

---
+++ 00E092.py
@@ -0,0 +1,23 @@
+#!/bin/bash
+#**************************************************************
+# Date: 081523 (Expected Solution with 28 Lines of Code)      *
+# Title: Is a Number Prime?                                   *
+# Status: In Progress (In Progress / Testing / Working)       *
+# A prime number is an integer greater than 1 that is only    *
+# divisible by one and itself. Write a function that          *
+# determines whether or not its parameter is prime, returning *
+# True if it is, and False otherwise. Write a main program    *
+# that reads an integer from the user and displays a message  *
+# indicating whether or not it is prime. Ensure that the main *
+# program will not run if the ﬁle containing your solution is *
+# imported into another program.                              *
+#                                                             *
+# Computed Result Validated:                                  *
+#**************************************************************
+#--------------------------------------------------------------
+def func_name(UserIn):
+   return
+#--------------------------------------------------------------
+if __name__ == "__main__":
+   print("Thank you for using this app.")
+#**************************************************************
