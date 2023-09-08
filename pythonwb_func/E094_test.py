270c35bd7142b02d655139b96c4133912f232389
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-09-08 08:55:55 +0000

    C0908231700
    - completed coding for exercise 94 now ready for testing

--- pythonwb_func/E094_test.py
+++ pythonwb_func/E094_test.py
@@ -2,14 +2,14 @@
 #**************************************************************
 # Date: 081523 (Expected Solution with 33 Lines of Code)      *
 # Title: Random Password                                      *
-# Status: In Progress (In Progress / Testing / Working)       *
+# Status: Testing (In Progress / Testing / Working)           *
 # Write a function that generates a random password. The      *
 # password should have a random length of between 7 and 10    *
 # characters. Each character should be randomly selected from *
 # positions 33 to 126 in the ASCII table. Your function will  *
 # not take any parameters. It will return the randomly        *
 # generated password as its only result. Display the randomly *
-# generated password in your ﬁle’s main program. Your main    *
+# generated password in your file’s main program. Your main   *
 # program should only run when your solution has not been     *
 # imported into another ﬁle. Hint: You will probably ﬁnd the  *
 # chr function helpful when completing this exercise.         *
@@ -18,10 +18,17 @@
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
+import random
 #--------------------------------------------------------------
-def func_name(UserIn):
-   return
+def ran_password():
+  final_value = ""
+  ctr = random.randint(7, 10)
+  while ctr != 0:
+    final_value += chr(random.randint(33, 126))
+    ctr -= 1
+  return final_value
 #--------------------------------------------------------------
 if __name__ == "__main__":
-   print("Thank you for using this app.")
+  print("The generated password is", ran_password())
+  print("Thank you for using this app.")
 #**************************************************************
