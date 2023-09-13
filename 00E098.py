b38bee071faa6d3585d6e005c140b5e60103cea2
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2023-09-13 11:47:36 -0400

    C0913232330 - started coding exercise 98.

--- 00E098.py
+++ 00E098.py
@@ -7,8 +7,8 @@
 # between hexadecimal digits (0, 1, 2, 3, 4, 5, 6, 7, 8, 9,   *
 # A, B, C, D, E and F) and base 10 integers. The hex2int      *
 # function is responsible for converting a string containing  *
-# a single hexadec- imal digit to a base 10 integer, while    *
-# the int2hex function is responsible for con- verting an     *
+# a single hexadecimal digit to a base 10 integer, while      *
+# the int2hex function is responsible for converting an       *
 # integer between 0 and 15 to a single hexadecimal digit.     *
 # Each function will take the value to convert as its only    *
 # parameter and return the converted value as the function’s  *
@@ -26,5 +26,6 @@ def int2hex(user_in):
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
+  input_data = input("Enter hexadecimal or decimal number: ")
   print("Thank you for using this app.")
 #**************************************************************
