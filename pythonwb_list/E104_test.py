f310a5d740e6e3390f2fc62d348de9f7719d7520
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-10-16 01:11:23 +0000

    C1015230930
    - completed coding for exercise 104

--- pythonwb_list/E104_test.py
+++ pythonwb_list/E104_test.py
@@ -2,7 +2,7 @@
 #**************************************************************
 # Date: 092723 (Expected Solution with 21 Lines of Code)      *
 # Title: Sorted Order                                         *
-# Status: In Progress (In Progress / Testing / Working)       *
+# Status: Testing (In Progress / Testing / Working)           *
 # Write a program that reads integers from the user and       *
 # stores them in a list. Your program should continue reading *
 # values until the user enters 0. Then it should display all  *
@@ -13,10 +13,21 @@
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
+usr_entry, ctr = -1, 0
+usr_ent_list = []
 #--------------------------------------------------------------
-def func_name(user_in):
-  return
+def sort_func(usr_in_list):
+  usr_in_list.sort()
+  return usr_in_list
 #--------------------------------------------------------------
 if __name__ == "__main__":
+  while usr_entry != 0:
+    usr_entry = int(input("Enter integers to be sorted. Enter 0 to end.: "))
+    if usr_entry != 0:
+      usr_ent_list.append(usr_entry)
+  sort_func(usr_ent_list)
+  while ctr <= (len(usr_ent_list) - 1):
+    print(usr_ent_list[ctr])
+    ctr += 1
   print("Thank you for using this app.")
 #**************************************************************
