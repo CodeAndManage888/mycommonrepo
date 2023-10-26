a812f051b87be7091e3fcc1b6056ff443c880e9b
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-10-26 04:51:42 +0000

    C1026231300
    - started coding exercise 108

--- 00E108.py
+++ 00E108.py
@@ -8,19 +8,19 @@
 # read your program should display all of the negative        *
 # numbers, followed by all of the zeros, followed by all of   *
 # the positive numbers. Within each group the numbers should  *
-# be displayed in the same order that they were entered This  *
-# copy belongs to 'acha04'Exercise 108: Negatives, Zeros and  *
-# Positives 51 by the user. For example, if the user enters   *
-# the values 3, -4, 1, 0,-1, 0, and-2 then your program       *
-# should output the values -4,-1,-2, 0, 0, 3, and 1. Your     *
-# program should display each value on its own line.          *
+# be displayed in the same order that they were entered       *
+# by the user. For example, if the user enters the values 3,  *
+# -4, 1, 0,-1, 0, and-2 then your program should output the   *
+# values -4,-1,-2, 0, 0, 3, and 1. Your program should        *
+# display each value on its own line.                         *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def func_name(user_in):
+def arrange_integer(user_list):
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
+  user_num = input(int("Enter a number (Negative, Zeroes and Positives): "))
   print("Thank you for using this app.")
 #**************************************************************
