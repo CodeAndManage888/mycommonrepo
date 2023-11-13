5e8a04dfc9e1bd4d2c291b183aeeafea1065f7e3
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-11-13 02:29:46 +0000

    C1113230930
    - created templates for exercise 117 to 120

---
+++ 00E119.py
@@ -0,0 +1,33 @@
+#!/bin/bash
+#**************************************************************
+# Date: 111323 (Expected Solution with 44 Lines of Code)      *
+# Title: Dealing Hands of Cards                               *
+# Status: In Progress (In Progress / Testing / Working)       *
+# In many card games each player is dealt a speciﬁc number of *
+# cards after the deck has been shufﬂed. Write a function,    *
+# deal , which takes the number of hands, the number of cards *
+# per hand, and a deck of cards as its three parameters. Your *
+# function should return a list containing all of the hands   *
+# that were dealt. Each hand will be represented as a list of *
+# cards. When dealing the hands, your function should modify  *
+# the deck of cards passed to it as a parameter, removing     *
+# each card from the deck as it is added to a player’s hand.  *
+# When cards are dealt, it is customary to give each player a *
+# card before any player receives an additional card. Your    *
+# function should follow this custom when constructing the    *
+# hands for the players. Use your solution to Exercise 118 to *
+# help you construct a main program that creates and shufﬂes  *
+# a deck of cards, and then deals out four hands of ﬁve cards *
+# each. Display all of the hands of cards, along with the     *
+# cards remaining in the deck after the hands have been       *
+# dealt.                                                      *
+#                                                             *
+# Computed Result Validated:                                  *
+#**************************************************************
+#--------------------------------------------------------------
+def func_name(user_in):
+  return
+#--------------------------------------------------------------
+if __name__ == "__main__":
+  print("Thank you for using this app.")
+#**************************************************************
