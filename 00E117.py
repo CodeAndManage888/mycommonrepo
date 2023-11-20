33cfb4984c890d4919ecdf4752896c159f964597
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-11-20 11:34:17 +0000

    C1120231930
    - continue coding exercise 117

--- 00E117.py
+++ 00E117.py
@@ -36,25 +36,32 @@
 # Computed Result Validated:                                  *
 #**************************************************************
 user_x, user_y = " ", " "
-sum_x, sum_y, sum_xy, sum_x_sq = 0, 0, 0, 0
 xy_coordinate = []
 #--------------------------------------------------------------
-def line_best_fit(user_in):
-  return
-def compute_m(user_in):
-  return
-def compute_b(user_in):
-  fin_val = ave_y - m*ave_x
-  return fin_val
+def line_best_fit(user_lst):
+  ctr, sum_x, sum_y, sum_xy, sum_x_squared, = 0, 0, 0, 0, 0
+  while ctr <= len(user_lst) - 1:
+    sum_x += float(user_lst[ctr])
+    sum_y += float(user_lst[ctr + 1])
+    sum_xy += float(user_lst[ctr]) * float(user_lst[ctr + 1])
+    ctr += 2
+  sum_x_squared = sum_x ** 2
+  num_data_pts = len(user_lst) / 2
+  comp_b_val = (sum_xy - (sum_x * sum_y) / num_data_pts) / (sum_x_squared - ((sum_x ** 2) / num_data_pts))
+  comp_m_val = (sum_y/num_data_pts) - (comp_b_val * (sum_x/num_data_pts))
+  return comp_m_val, comp_b_val
 #--------------------------------------------------------------
 if __name__ == "__main__":
   while user_x != "":
     user_x = input("Enter x coordinates: ")
-    if user_x != "" and int(user_x) >= 0:
+    if user_x != "" and float(user_x) >= 0:
       xy_coordinate.append(user_x)
       while user_y == "" or user_y == " ":
         user_y = input("Enter y coordinates: ")
       xy_coordinate.append(user_y)
+      user_x, user_y = " ", " "
   print(xy_coordinate)
+  fin_m_val, fin_b_val = line_best_fit(xy_coordinate)
+  print(f"y = {fin_m_val:.2f}x + {fin_b_val:.2f}")
   print("Thank you for using this app.")
 #**************************************************************
