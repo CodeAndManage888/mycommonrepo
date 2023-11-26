d6038f851a6c83ef391a06f605b93f3a3f18f9ba
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-11-26 14:52:23 +0000

    C1126232300
    - completed coding for exercise 120

--- pythonwb_list/E120_test.py
+++ pythonwb_list/E120_test.py
@@ -2,7 +2,7 @@
 #**************************************************************
 # Date: 111323 (Expected Solution with 41 Lines of Code)      *
 # Title: Is a List already in Sorted Order?                   *
-# Status: In Progress (In Progress / Testing / Working)       *
+# Status: Testing (In Progress / Testing / Working)           *
 # Write a function that determines whether or not a list of   *
 # values is in sorted order (either ascending or descending). *
 # The function should return True if the list is already      *
@@ -16,9 +16,14 @@
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def sorted_list(user_in):
-  return
+def sorted_list(data_list):
+  # Determine if the list is already sorted.
+  return data_list == sorted(data_list)
 #--------------------------------------------------------------
 if __name__ == "__main__":
+  user_in = input("Enter a list of numbers separated by commas: ")
+  user_in = user_in.split(",")
+  user_in = [int(x) for x in user_in]
+  print("Is the list already sorted? ", sorted_list(user_in ))
   print("Thank you for using this app.")
 #**************************************************************
