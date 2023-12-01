27c4f99ae7c724163353d49340d05311f32c79b1
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-12-01 09:38:41 +0000

    C1201231730
    - started coding exercise 122

--- 00E122.py
+++ 00E122.py
@@ -11,39 +11,40 @@
 # required to determine where one token ends and the next one *
 # begins. In a mathematical expression, tokens are items such *
 # as operators, numbers and parentheses. Some tokens, such as *
-# *,/,ˆ,(and)are easy to identify because the token is a      *
+# *, /, ˆ, ( and ) are easy to identify because the token is a*
 # single character, and the character is never part of        *
-# another token. The +and -symbols are a little bit more      *
+# another token. The + and - symbols are a little bit more    *
 # challenging to handle because they might represent the      *
 # addition or subtraction operator, or they might be part of  *
-# a number token. Hint: A+or-is an operator if the            *
+# a number token. Hint: A + or - is an operator if the        *
 # non-whitespace character immediately before it is part of a *
 # number, or if the non-whitespace character immediately      *
 # before it is a close parenthesis. Otherwise it is part of a *
 # number. Write a function that takes a string containing a   *
 # mathematical expression as its only parameter and breaks it *
 # into a list of tokens. Each token should be a parenthesis,  *
-# an operator, or a number with an optional leading +or-(for  *
-# simplicity we will only work with integers in this          *
+# an operator, or a number with an optional leading + or - (  *
+# for simplicity we will only work with integers in this      *
 # problem). Return the list of tokens as the function’s       *
 # result. You may assume that the string passed to your       *
 # function always contains a valid mathematical expression    *
-# consisting of parentheses, operators and integers. How-     *
-# ever, your function must handle variable amounts of         *
-# whitespace between these elements. Include a main program   *
-# that demonstrates your tokenizing function by reading an    *
+# consisting of parentheses, operators and integers. However, *
+# your function must handle variable amounts of whitespace    *
+# between these elements. Include a main program that         *
+# demonstrates your tokenizing function by reading an         *
 # expression from the user and printing the list of tokens.   *
-# Ensure that the This copy belongs to 'acha04'Exercise       *
-# 122:Tokenizing a String 57 main program will not run when   *
-# the ﬁle containing your solution is imported into another   *
-# program.                                                    *
+# Ensure that the main program will not run when the file     *
+# containing your solution is imported into another program.  *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def func_name(user_in):
-  return
+def token_func(user_in):
+  user_in_list = user_in.split()
+  return user_in_list
 #--------------------------------------------------------------
 if __name__ == "__main__":
+  math_exp = input("Enter a mathematical expression: ")
+  print(token_func(math_exp))
   print("Thank you for using this app.")
 #**************************************************************
