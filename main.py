7dad63626c38aacc716fa181382d9e386eb7aa79
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2023-12-29 15:03:53 +0000

    C1229232300
    - wrote sample code from solo learn lesson - lambdas

--- main.py
+++ main.py
@@ -1,68 +1,100 @@
-#!/bin/bash
-#**************************************************************
-# Date: 112423 (Expected Solution with 64 Lines of Code)      *
-# Title: Tokenizing a String                                  *
-# Status: Testing (In Progress / Testing / Working)           *
-# Tokenizing is the process of converting a string into a     *
-# list of substrings, known as tokens. In many circumstances, *
-# a list of tokens is far easier to work with than the        *
-# original string because the original string may have        *
-# irregular spacing. In some cases substantial work is also   *
-# required to determine where one token ends and the next one *
-# begins. In a mathematical expression, tokens are items such *
-# as operators, numbers and parentheses. Some tokens, such as *
-# *, /, ˆ, ( and ) are easy to identify because the token is a*
-# single character, and the character is never part of        *
-# another token. The + and - symbols are a little bit more    *
-# challenging to handle because they might represent the      *
-# addition or subtraction operator, or they might be part of  *
-# a number token. Hint: A + or - is an operator if the        *
-# non-whitespace character immediately before it is part of a *
-# number, or if the non-whitespace character immediately      *
-# before it is a close parenthesis. Otherwise it is part of a *
-# number. Write a function that takes a string containing a   *
-# mathematical expression as its only parameter and breaks it *
-# into a list of tokens. Each token should be a parenthesis,  *
-# an operator, or a number with an optional leading + or - (  *
-# for simplicity we will only work with integers in this      *
-# problem). Return the list of tokens as the function’s       *
-# result. You may assume that the string passed to your       *
-# function always contains a valid mathematical expression    *
-# consisting of parentheses, operators and integers. However, *
-# your function must handle variable amounts of whitespace    *
-# between these elements. Include a main program that         *
-# demonstrates your tokenizing function by reading an         *
-# expression from the user and printing the list of tokens.   *
-# Ensure that the main program will not run when the file     *
-# containing your solution is imported into another program.  *
-#                                                             *
-# Computed Result Validated:                                  *
-#**************************************************************
-symbol = ["*", "/", "(", ")"]
-#--------------------------------------------------------------
-def token_func(math_str):
-  token_list = []
-  no_spaces_list = math_str.split()
-  print("Input: ", no_spaces_list)
-  for exp in no_spaces_list:
-    if len(exp) == 1:
-      token_list.append(exp)
-    else:
-      signed_char = ""
-      for char in exp:
-        if char in symbol:
-          if len(signed_char) != 0:
-            token_list.append(signed_char)
-            signed_char = ""
-          token_list.append(char)
-        elif char == "+" or char == "-" or char.isdigit():
-          signed_char += char
-      if signed_char != "":
-        token_list.append(signed_char)
-  return token_list
-#--------------------------------------------------------------
-if __name__ == "__main__":
-  math_exp = input("Enter a mathematical expression: ")
-  print(token_func(math_exp))
-  print("Thank you for using this app.")
-#**************************************************************
+#-----------------------------------------------------------------------------------
+triple = lambda x: x * 3
+add = lambda x, y: x + y
+print(add(triple(3), 4))
+#-----------------------------------------------------------------------------------
+#-----------------------------------------------------------------------------------
+'''
+#-----------------------------------------------------------------------------------
+def polynomial(x):
+    return x**2 + 5*x + 4
+print(polynomial(5))
+
+print((lambda x: x**2 + 5*x + 4)(5))
+
+#-----------------------------------------------------------------------------------
+def my_func(f, arg):
+  return f(arg)
+
+x = int(input())
+
+print(my_func(lambda x: 2*x*x, x))
+#-----------------------------------------------------------------------------------
+txt = input("Enter a string: ")
+word_list = txt.split()
+long_word = ""
+for word in word_list:
+  if len(word) >= len(long_word):
+    long_word = word
+print(long_word)
+#import site
+#print(site.getsitepackages())
+#x = 1
+#print(x == 1)
+#user_input = " "
+#proc_data_one = user_input.strip()
+#print(ord("A"), ord("Z"), ord("a"), ord("z"), ord("0"), ord("9"))
+#digit_list = [2, 4, 5, "A", "F", "B"]
+#print(digit_list.sort())
+#count = len([ltr in ltr for ltr in "ABADZFAHIJKLMNOPQRSTUVWXYZ" if ltr == "A" and ltr == "Z"])
+#def greet(name):
+'''
+'''
+#print("Hello, " + name)
+#-----------------------------------------------------------------------------------
+#Algorithm 1: Fibonacci using Recurssion
+#def fib1(n: int) -> int:
+#  if n < 2:
+#    print("condition 1")
+#    return n
+#  else:
+#    print("condition 2")
+#    return fib1(n - 1) + fib1(n - 2)
+#if __name__ == "__main__":
+#  user_num = int(input("Enter a number: "))
+#  print(fib1(user_num))
+#-----------------------------------------------------------------------------------
+#Algorith 2: Fibonacci
+#def comp(n):
+#  for i in range(n):
+#    if i < 2:
+#      print(i)
+#    else:
+#      print(i+1)
+#  return
+#if __name__ == "__main__":
+#  user_num = int(input("Enter a number: "))
+#  comp(user_num)
+#-----------------------------------------------------------------------------------
+#price = {"Nachos":6, "Pizza":6, "Cheeseburger":10, "Water":4, "Coke":5}
+#olist = []
+#tax = .07
+#total = 0
+
+#order = input()
+#olist = order.split()
+#for o in olist:
+#    total += price.get(o,5)
+#total += total*tax
+#f_total = f"{total:.2f}"
+#print(f_total)
+#-----------------------------------------------------------------------------------
+def add(x,y):
+  print(add(x + y,x*y))
+  return
+func = add
+func(1,2)
+test run
+#-----------------------------------------------------------------------------------
+pyfile = open("00E122.py", "r")
+for num in range(0,100):
+  print(pyfile.read(5))
+pyfile.close()
+#-----------------------------------------------------------------------------------
+with open("00E122.py", "r") as code:
+  for num in range(0,10):
+    print(code.read(5))
+#-----------------------------------------------------------------------------------
+squares = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+print(squares[1:-1])
+'''
