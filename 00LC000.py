b4bd6ff33917e357f26e3010a2e31b91a6ebc6e2
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2024-01-16 13:25:53 +0000

    C011622130
    - wrote a new code to solve leet code problems.

---
+++ 00LC000.py
@@ -0,0 +1,13 @@
+# ------------------------------------------------------------------------
+# Given an array of integers that is already sorted in ascending order, 
+# find two numbers such that they add up to a specific target number.
+# The function twoSum should return indices of the two numbers such that 
+# they add up to the target, where index1 must be less than index2 are not
+# zero-based.
+# Note: You may assume that each input would have exactly one solution.
+# Input: numbers={2, 7, 11, 15}, target=9 Outpu: [1,2]
+# ------------------------------------------------------------------------
+# ------------------------------------------------------------------------
+# Given a linked list, determine if it has a cycle in it.
+# Follow up: Can you solve it without using extra space?
+# ------------------------------------------------------------------------
