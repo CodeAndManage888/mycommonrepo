ae9d80befbff3b81b1645f9f92ca161fd3129ba8
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2024-01-17 14:33:50 +0000

    C0117242230
    - coded the first Leet Code question available.

--- 00LC000.py
+++ 00LC000.py
@@ -7,6 +7,26 @@
 # Note: You may assume that each input would have exactly one solution.
 # Input: numbers={2, 7, 11, 15}, target=9 Outpu: [1,2]
 # ------------------------------------------------------------------------
+def twoSum(numbers, target):
+    # Write your code here
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i] + numbers[j] == target:
+                return [i, j]
+    return []
+# ------------------------------------------------------------------------
+# The main function
+# ------------------------------------------------------------------------
+if __name__ == "__main__":
+    # Read in a list of integers
+    line = input().strip()
+    numbers = [int(x) for x in line.split(',')]
+    # Sort the input list
+    numbers.sort()
+    # Read in the target
+    target = int(input())
+    # Find the two numbers that add up to the target
+    print(twoSum(numbers, target))
 # ------------------------------------------------------------------------
 # Given a linked list, determine if it has a cycle in it.
 # Follow up: Can you solve it without using extra space?
