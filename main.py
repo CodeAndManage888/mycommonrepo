039f75392e6e33a4d6ec96fd42bf9d2042f12868
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2024-02-15 15:15:58 +0000

    C0215242330
    -generate new exercise templates

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-#!/bin/bash
-#**************************************************************
-# Date: 112623 (Expected Solution with 40 Lines of Code)      *
-# Title: Generate All Sublists of a List                      *
-# Status: In Progress (In Progress / Testing / Working)       *
-# Using the deﬁnition of a sublist from Exercise 125, write a *
-# function that returns a list containing every possible      *
-# sublist of a list. For example, the sublists of [1, 2, 3]   *
-# are[],[1],[2],[3],[1, 2] ,[2, 3] and [1, 2, 3] . Note that  *
-# your function will always return a list containing at       *
-# least the empty list because the empty list is a sublist of *
-# every list. Include a main program that demonstrate your    *
-# function by displaying all of the sublists of several       *
-# different lists.                                            *
-#                                                             *
-# Computed Result Validated:                                  *
-#**************************************************************
-#--------------------------------------------------------------
-def extract_sublst(olist):
-  nlist = []
-  ilist = []
-  glist = []
-  ctrgrp_rng = 0
-  while ctrgrp_rng <= len(olist) - 2:
-    glist.append(ctrgrp_rng)
-    ctrgrp_rng += 1
-  print(ctrgrp_rng)
-  stop_mark = len(olist) - 1
-  nlist.append(ilist)
-  return nlist
-#--------------------------------------------------------------
-if __name__ == "__main__":
-  sublist_in = input("Enter a first list of numbers separated by spaces: ")
-  sublist = sublist_in.split()
-  print(extract_sublst(sublist))
-  print("Original List: ", sublist)
-  print("Thank you for using this app.")
-#**************************************************************
+import PyPDF2
+import os
+import datetime
+
+def read_pdf(file_path, page_exer_nostr):
+    target_list = page_exer_nostr.split()
+    page_ctr = len(target_list) - 1
+    with open(file_path, 'rb') as file:
+        # Create a PDF reader object
+        pdf_reader = PyPDF2.PdfReader(file)
+
+        # Get the total number of pages in the PDF
+        num_pages = len(pdf_reader.pages)                                                # Do I still need this?
+
+        # Get the text data
+        idx4 = 1
+        text = ""
+        while page_ctr != 0:
+            page = pdf_reader.pages[int(target_list[idx4])]
+            page_ctr -= 1
+            idx4 += 1
+            text += page.extract_text()
+#        print(text)                                                                     # Uncomment for debugging
+
+        # Prepare the extraction of the target exercise only.
+        content = text.split("\n")
+#        print(content)                                                                  # Uncomment for debugging
+        start_txt = "Exercise " + target_list[0] + ":"
+        end_txt = "Exercise " + str(int(target_list[0]) + 1) + ":"
+#        print(start_txt)                                                                # Uncomment for debugging
+#        print(end_txt)                                                                  # Uncomment for debugging
+        return_block = []
+        start_pos_list = []
+        end_pos_list = []
+
+        # This line of codes will will identify the title and other starting/ending details.
+        start_flag = False
+        for idx1, line in enumerate(content):
+            startpos = line.find(start_txt)
+            if startpos != -1 and not start_flag:
+                start_pos_list.append(idx1)
+                start_flag = True
+            endpos = line.find(end_txt)
+            if endpos != -1:
+                end_pos_list.append(idx1)
+                break
+            else:
+                endpos = line.find("This copy belongs to")
+                if endpos != -1 and len(target_list) == 2:
+                    end_pos_list.append(idx1)
+                    break
+
+#        print(start_pos_list)                                                           # Uncomment for debugging
