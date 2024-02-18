69eda4c16ab00113213c0605b264ab25e5b27980
CodeAndManage888 <108861194+CodeAndManage888@users.noreply.github.com>
2024-02-18 15:02:26 +0000

    C0218242300
    - continue coding exercise 126

--- 00E126.py
+++ 00E126.py
@@ -6,7 +6,7 @@
 # Using the deﬁnition of a sublist from Exercise 125, write a *
 # function that returns a list containing every possible      *
 # sublist of a list. For example, the sublists of [1, 2, 3]   *
-# are[],[1],[2],[3],[1, 2] ,[2, 3] and [1, 2, 3] . Note that  *
+# are [],[1],[2],[3],[1, 2] ,[2, 3] and [1, 2, 3] . Note that *
 # your function will always return a list containing at       *
 # least the empty list because the empty list is a sublist of *
 # every list. Include a main program that demonstrate your    *
