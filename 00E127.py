30dbb8a064a4bdbe1c5178c65a8a2e8146394905
boredmanager888 <lowenddevice888@gmail.com>
2024-02-23 11:55:07 +0800

    C0223241200 - started coding exercise 127 via raspberry pi 400.

--- 00E127.py
+++ 00E127.py
@@ -8,13 +8,13 @@
 # numbers between 2 and some limit, say 100. A description of *
 # the algorithm follows: Write down all of the numbers from 0 *
 # to the limit Cross out 0 and 1 because they are not prime   *
-# Setpequal to 2 While pis less than the limit do Cross out   *
-# all multiples of p(but not pitself) Setpequal to the next   *
+# Set p equal to 2 While p is less than the limit do Cross out*
+# all multiples of p (but not pitself) Set p equal to the next*
 # number in the list that is not crossed out Report all of    *
 # the numbers that have not been crossed out as prime The key *
 # to this algorithm is that it is relatively easy to cross    *
 # out every nth number on a piece of paper. This is also an   *
-# easy task for a computer—a for loop can simulate this       *
+# easy task for a computer a for loop can simulate this       *
 # behavior when a third parameter is provided to the range    *
 # function. When a number is crossed out, we know that it is  *
 # no longer prime, but it still occupies space on the piece   *
