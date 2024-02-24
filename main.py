5e50849fbe0a16bef206f40faead413c1203f396
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-02-24 15:47:21 +0000

    C0224242330
    - continue coding exercise 127

--- main.py
+++ main.py
@@ -1,44 +1,32 @@
 #!/bin/bash
 #**************************************************************
-# Date: 112623 (Expected Solution with 40 Lines of Code)      *
-# Title: Generate All Sublists of a List                      *
+# Date: 112823 (Expected Solution with 33 Lines of Code)      *
+# Title: The Sieve of Eratosthenes                            *
 # Status: In Progress (In Progress / Testing / Working)       *
-# Using the deﬁnition of a sublist from Exercise 125, write a *
-# function that returns a list containing every possible      *
-# sublist of a list. For example, the sublists of [1, 2, 3]   *
-# are [],[1],[2],[3],[1, 2] ,[2, 3] and [1, 2, 3] . Note that *
-# your function will always return a list containing at       *
-# least the empty list because the empty list is a sublist of *
-# every list. Include a main program that demonstrate your    *
-# function by displaying all of the sublists of several       *
-# different lists.                                            *
+# The Sieve of Eratosthenes is a technique that was developed *
+# more than 2,000 years ago to easily ﬁnd all of the prime    *
+# numbers between 2 and some limit, say 100. A description of *
+# the algorithm follows: Write down all of the numbers from 0 *
+# to the limit Cross out 0 and 1 because they are not prime   *
+# Setpequal to 2 While pis less than the limit do Cross out   *
+# all multiples of p(but not pitself) Setpequal to the next   *
+# number in the list that is not crossed out Report all of    *
+# the numbers that have not been crossed out as prime The key *
+# to this algorithm is that it is relatively easy to cross    *
+# out every nth number on a piece of paper. This is also an   *
+# easy task for a computer—a for loop can simulate this       *
+# behavior when a third parameter is provided to the range    *
+# function. When a number is crossed out, we know that it is  *
+# no longer prime, but it still occupies space on the piece   *
+# of paper, and must still be considered when computing later *
+# prime numbers.                                              *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def extract_sublst(olist):
-  nlist, glist = [], []
-  ctr1, ctr2 = 0, 0
-  while ctr1 <= len(olist) - 1:
-    glist.append(ctr1)
-    ctr1 += 1
-  print(glist)
-  for g in glist:
-    if g == 0:
-      nlist.append([])
-    else:
-      while len(olist) >= ctr2 and ctr2 + g <= len(olist):
-        nlist.append(olist[ctr2:ctr2 + g])
-        ctr2 += 1
-      ctr2 = 0
-  if olist != []:
-    nlist.append(olist)
-  return nlist
+def func_name(user_in):
+  return
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  sublist_in = input("Enter a first list of numbers separated by spaces: ")
-  sublist = sublist_in.split()
-  print("Original List: ", sublist)
-  print("Extracted Sublists: ", extract_sublst(sublist))
   print("Thank you for using this app.")
 #**************************************************************
