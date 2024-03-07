e709003ee2467f266da26b48ede29e9be72bf9fe
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-03-07 04:11:58 +0000

    C0307241230
    - continue coding exercise 128

--- main.py
+++ main.py
@@ -15,16 +15,22 @@
 # it returns multiple keys, a single key, and no keys. Ensure *
 # that your main program only runs when the ﬁle containing    *
 # your solution to this exercise has not been imported into   *
-# another program. © Springer International Publishing        *
-# Switzerland 2014 B. Stephenson, The Python Workbook , DOI   *
-# 10.1007/978-3-319-14240-1_661                               *
+# another program.                                            *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def func_name(user_in):
+def reverseLookup(dict_a, user_in):
+  user_in = user_in.lower()
+  for key in dict_a:
+    if user_in == dict_a[key]:
+      print(key)
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
+  print("This is the main program.")
+  mainDictionary = {"a": "apple", "b": "banana", "c": "carrot", "d": "dog", "e": "elephant", "f": "frog", "g": "grape", "h": "horse", "i": "ice cream", "j": "jelly", "k": "kangaroo", "l": "lemon", "m": "mango", "n": "narwhal", "o": "orange", "p": "pineapple", "q": "quail", "r": "rabbit", "s": "strawberry", "t": "tiger", "u": "umbrella", "v": "vulture", "w": "whale", "x": "x-ray", "y": "yak", "z": "zebra"}
+  ValueSearch = input("Enter a value to search for: ")
+  print(reverseLookup(mainDictionary, ValueSearch))
   print("Thank you for using this app.")
 #**************************************************************
