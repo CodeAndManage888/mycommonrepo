d2b39a96317decc15c48103180ed423cd9414a9c
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-03-08 13:26:37 +0000

    C0308242130
    - completed coding exercise 128

--- pythonwb_dict/E128_test.py
+++ pythonwb_dict/E128_test.py
@@ -2,7 +2,7 @@
 #**************************************************************
 # Date: 120723 (Expected Solution with 40 Lines of Code)      *
 # Title: Reverse Lookup                                       *
-# Status: In Progress (In Progress / Testing / Working)       *
+# Status: Testing (In Progress / Testing / Working)           *
 # Write a function named reverseLookup that ﬁnds all of the   *
 # keys in a dictionary that map to a speciﬁc value. The       *
 # function will take the dictionary and the value to search   *
@@ -22,14 +22,15 @@
 #--------------------------------------------------------------
 def reverseLookup(dict_a, user_in):
   user_in = user_in.lower()
+  common_key = []
   for key in dict_a:
     if user_in == dict_a[key]:
-      print(key)
-  return
+      common_key.append(key)
+  return common_key
 #--------------------------------------------------------------
 if __name__ == "__main__":
   print("This is the main program.")
-  mainDictionary = {"a": "apple", "b": "banana", "c": "carrot", "d": "dog", "e": "elephant", "f": "frog", "g": "grape", "h": "horse", "i": "ice cream", "j": "jelly", "k": "kangaroo", "l": "lemon", "m": "mango", "n": "narwhal", "o": "orange", "p": "pineapple", "q": "quail", "r": "rabbit", "s": "strawberry", "t": "tiger", "u": "umbrella", "v": "vulture", "w": "whale", "x": "x-ray", "y": "yak", "z": "zebra"}
+  mainDictionary = {"a": "apple", "b": "banana", "c": "carrot", "d": "dog", "e": "elephant", "f": "frog", "g": "grape", "h": "horse", "i": "ice cream", "j": "jelly", "k": "kangaroo", "l": "lemon", "m": "mango", "n": "narwhal", "o": "orange", "p": "pineapple", "q": "quail", "r": "rabbit", "s": "strawberry", "t": "tiger", "u": "umbrella", "v": "vulture", "w": "whale", "x": "carrot", "y": "yak", "z": "zebra"}
   ValueSearch = input("Enter a value to search for: ")
   print(reverseLookup(mainDictionary, ValueSearch))
   print("Thank you for using this app.")
