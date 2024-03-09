70f06ef9e4682ec5a68eb0d9f6d4067fba0a5773
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-03-09 15:08:14 +0000

    C0309242300
    - started coding exercise 129

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
 #!/bin/bash
 #**************************************************************
-# Date: 120723 (Expected Solution with 40 Lines of Code)      *
-# Title: Reverse Lookup                                       *
-# Status: Testing (In Progress / Testing / Working)           *
-# Write a function named reverseLookup that ﬁnds all of the   *
-# keys in a dictionary that map to a speciﬁc value. The       *
-# function will take the dictionary and the value to search   *
-# for as its only parameters. It will return a (possibly      *
-# empty) list of keys from the dictionary that map to the     *
-# provided value. Include a main program that demonstrates    *
-# the reverseLookup function as part of your solution to this *
-# exercise. Your program should create a dictionary and then  *
-# show that the reverseLookup function works correctly when   *
-# it returns multiple keys, a single key, and no keys. Ensure *
-# that your main program only runs when the ﬁle containing    *
-# your solution to this exercise has not been imported into   *
-# another program.                                            *
+# Date: 022624 (Expected Solution with 42 Lines of Code)      *
+# Title: Two Dice Simulation                                  *
+# Status: In Progress (In Progress / Testing / Working)       *
+# In this exercise you will simulate 1,000 rolls of two dice. *
+# Begin by writing a function that simulates rolling a pair   *
+# of six-sided dice. Your function will not take any          *
+# parameters. It will return the total that was rolled on two *
+# dice as its only result. Write a main program that uses     *
+# your function to simulate rolling two six-sided dice 1,000  *
+# times. As your program runs, it should count the number of  *
+# times that each total occurs. Then it should display a      *
+# table that summarizes this data. Express the frequency for  *
+# each total as a percentage of the total number of rolls.    *
+# Your program should also display the percentage expected by *
+# probability theory for each total. Sample output is shown   *
+# below.                                                      *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def reverseLookup(dict_a, user_in):
-  user_in = user_in.lower()
-  common_key = []
-  for key in dict_a:
-    if user_in == dict_a[key]:
-      common_key.append(key)
-  return common_key
+def rollDice():
+  import random
+  return random.randint(1, 6) + random.randint(1, 6)
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  print("This is the main program.")
-  mainDictionary = {"a": "apple", "b": "banana", "c": "carrot", "d": "dog", "e": "elephant", "f": "frog", "g": "grape", "h": "horse", "i": "ice cream", "j": "jelly", "k": "kangaroo", "l": "lemon", "m": "mango", "n": "narwhal", "o": "orange", "p": "pineapple", "q": "quail", "r": "rabbit", "s": "strawberry", "t": "tiger", "u": "umbrella", "v": "vulture", "w": "whale", "x": "carrot", "y": "yak", "z": "zebra"}
-  ValueSearch = input("Enter a value to search for: ")
-  print(reverseLookup(mainDictionary, ValueSearch))
+  print("This is main program.")
+  rolls = 1000
+  roll_count = [0] * 11
+  for i in range(rolls):
+    roll_count[rollDice() - 2] += 1
+  print("Roll\tCount\tExpected\tActual")
+  for i in range(11):
+    print(f"{i + 2}\t{roll_count[i]}\t{roll_count[i] / rolls * 100:.2f}%\t{(i + 2) / 36 * 100:.2f}%")
   print("Thank you for using this app.")
 #**************************************************************
