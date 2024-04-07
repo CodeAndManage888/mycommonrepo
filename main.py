69fd074a4ce4be4e1301f0b7c127eb2b6f58e65d
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-04-07 15:27:20 +0000

    C0407242330
    - started coding exercise 137

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 #!/bin/bash
 #**************************************************************
-# Date: 032724 (Expected Solution with 48 Lines of Code)      *
-# Title: Anagrams Again                                       *
-# Status: Testing (In Progress / Testing / Working)           *
-# The notion of anagrams can be extended to multiple words.   *
-# For example, “William Shakespeare” and “I am a weakish      *
-# speller” are anagrams when capitalization and spacing are   *
-# ignored. Extend your program from Exercise 135 so that it   *
-# is able to check if two phrases are anagrams. Your program  *
-# should ignore capitalization, punctuation marks and spacing *
-# when making the determination.                              *
+# Date: 032724 (Expected Solution with 18 Lines of Code)      *
+# Title: Scrabble™ Score                                      *
+# Status: In Progress (In Progress / Testing / Working)       *
+# In the game of Scrabble™, each letter has points associated *
+# with it. The total score of a word is the sum of the scores *
+# of its letters. More common letters are worth fewer points  *
+# while less common letters are worth more points. The points *
+# associated with each letter are shown below: One point A,   *
+# E, I, L, N, O, R, S, T and U Two points Da n dG Three       *
+# points B, C, M and P Four points F, H, V, W and Y Five      *
+# points K Eight points Ja n dX Ten points Qa n dZ Write a    *
+# program that computes and displays the Scrabble™ score for  *
+# a word. Create a dictionary that maps from letters to point *
+# values. Then use the dictionary to compute the score. A     *
+# Scrabble™ board includes some squares that multiply the     *
+# value of a letter or the value of an entire word. We will   *
+# ignore these squares in this exercise.                      *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def anagram_chk(user_str):
-  anagram_dict = {}
-  for char in user_str:
-    if char in anagram_dict:
-      anagram_dict[char] += 1
-    else:
-      anagram_dict[char] = 1
-  print(anagram_dict)
-  return anagram_dict
+def func_name(user_in):
+  return
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  user_str1 = input("Enter the first string: ")
-  clean_user_str1 = user_str1.lower()
-  user_str2 = input("Enter the second string: ")
-  clean_user_str2 = user_str2.lower()
-  if anagram_chk(clean_user_str1.replace(" ", "")) == anagram_chk(clean_user_str2.replace(" ", "")):
-    print("The two strings are anagrams.")
-  else:
-    print("The two strings are not anagrams.")
   print("Thank you for using this app.")
 #**************************************************************
