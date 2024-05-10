988ed097ce50b502401fc60214bd6a0063aafbc1
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-05-10 11:06:03 +0000

    C0510241930
    - generated templates for exercise 150 and 151

---
+++ 00E150.py
@@ -0,0 +1,28 @@
+#!/bin/bash
+#**************************************************************
+# Date: 051024 (Expected Solution with 46 Lines of Code)      *
+# Title: Remove Comments                                      *
+# Status: In Progress (In Progress / Testing / Working)       *
+# Python uses the #character to mark the beginning of a       *
+# comment. The comment ends at the end of the line containing *
+# the #character. In this exercise, you will create a program *
+# that removes all of the comments from a Python source ﬁle.  *
+# Check each line in the ﬁle to determine if a #character is  *
+# present. If it is then your program should remove all of    *
+# the characters from the #character to the end of the line   *
+# (we’ll ignore the situation where the comment character     *
+# occurs inside of a string). Save the modiﬁed ﬁle using a    *
+# new name that will be entered by the user. The user will    *
+# also enter the name of the input ﬁle. Ensure that an        *
+# appropriate error message is displayed if a problem is      *
+# encountered while accessing the ﬁles.                       *
+#                                                             *
+# Computed Result Validated:                                  *
+#**************************************************************
+#--------------------------------------------------------------
+def func_name(user_in):
+  return
+#--------------------------------------------------------------
+if __name__ == "__main__":
+  print("Thank you for using this app.")
+#**************************************************************
