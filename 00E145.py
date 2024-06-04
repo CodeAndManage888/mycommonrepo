3eed85c177e07db38d3778462d3145105735f53c
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-06-04 14:53:07 +0000

    C0604242300
    - continue coding exercise 145.

--- 00E145.py
+++ 00E145.py
@@ -2,7 +2,7 @@
 #**************************************************************
 # Date: 051224 (Expected Solution with 39 Lines of Code)      *
 # Title: Find the Longest Word in a File                      *
-# Status: In Progress (In Progress / Testing / Working)       *
+# Status: Testing (In Progress / Testing / Working)           *
 # In this exercise you will create a Python program that      *
 # identifies the longest word(s) in a file. Your program      *
 # should output an appropriate message that includes the      *
@@ -14,9 +14,24 @@
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def func_name(user_in):
-  return
-#--------------------------------------------------------------
 if __name__ == "__main__":
+  user_in = input("Enter the file name: ")
+  word_len = 0
+  word_list = []
+  with open(user_in, "r") as file_handle:
+    for line in file_handle:
+        print(line)
+        word_dump = line.split()
+        for word in word_dump:
+            print(word)
+            if len(word) > word_len:
+                word_len = len(word)
+                word_list.clear()
+                word_list.append(word)
+            elif len(word) == word_len:
+                word_list.append(word)
+            else:
+                continue
+  print("The longest word(s) in the file is/are:", word_list, "with a length of", word_len)
   print("Thank you for using this app.")
 #**************************************************************
