f5037cc8fbd46fb8edf908da9444567ffb25e8f9
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-06-07 08:22:11 +0000

    C0607241630
    - completed exercise 146 and started exercise 147

--- 00E147.py
+++ 00E147.py
@@ -4,16 +4,15 @@
 # Title: Words that Occur Most                                *
 # Status: In Progress (In Progress / Testing / Working)       *
 # Write a program that displays the word (or words) that      *
-# occur most frequently in a ﬁle. Your program should begin   *
-# by reading the name of the ﬁle from the user. Then it       *
-# should ﬁnd the word(s) by splitting each line in the ﬁle at *
-# each space. Finally, any leading or trailing punctuation    *
+# occur most frequently in a file. Your program should begin  *
+# by reading the name of the file from the user. Then it      *
+# should find the word(s) by splitting each line in the file  *
+# at each space. Finally, any leading or trailing punctuation  *
 # marks should be removed from each word. In addition, your   *
 # program should ignore capitalization. As a result, apple    *
-# ,apple! , Apple andApPlE should all be treated as the same  *
-# word. You will probably ﬁnd your solution to Exercise 111   *
-# helpful when completing this problem. This copy belongs to  *
-# 'acha04'72 7 File and Exception Exercises                   *
+# ,apple! , Apple and ApPlE should all be treated as the same *
+# word. You will probably find your solution to Exercise 111  *
+# helpful when completing this problem.                       *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
