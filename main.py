ef70638a778d1f8a18b7ffe300875aedea02461a
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-06-12 12:34:11 +0000

    C0612242100
    - started solving leet code problem 7

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-def chk_pal(user_in):
-  if user_in == user_in[::-1]:
-    return True
-  else:
-    return False
+  def reverse_integer(user_in):
+    val1 = "01111111111111111111111111111111"
+    if user_in[::-1] == user_in:
+      return user_in[::-1]
+    else:
+      return 0
 
-if __name__ == "__main__":
-  input_integer = input("Enter a number: ")
-  print("The number is a palindrome:", chk_pal(input_integer))
-  print("Thank you for using this app.")
+  if __name__ == "__main__":
+    signed_32bit_integer = input("Enter a signed 32 bit integer: ")
+    print("The reverse of the input is:", reverse_integer(signed_32bit_integer))
+    print("Thank you for using this app.")
