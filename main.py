c0dae4cb3bff230afb27df18a88ea71c6a12686e
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-06-20 11:19:41 +0000

    C0620241930
    - competed coding exercise 149 and started exercise 150

--- main.py
+++ main.py
@@ -1,28 +1,28 @@
 #!/bin/bash
 #**************************************************************
-# Date: 050924 (Expected Solution with 23 Lines of Code)      *
-# Title: Number the Lines in a File                           *
+# Date: 051024 (Expected Solution with 46 Lines of Code)      *
+# Title: Remove Comments                                      *
 # Status: In Progress (In Progress / Testing / Working)       *
-# Create a program that adds line numbers to a file. The name *
-# of the input file will be read from the user, as will the   *
-# name of the new file that your program will create. Each    *
-# line in the output file should begin with the line number,  *
-# followed by a colon and a space, followed by the line from  *
-# the input file.                                             *
+# Python uses the #character to mark the beginning of a       *
+# comment. The comment ends at the end of the line containing *
+# the #character. In this exercise, you will create a program *
+# that removes all of the comments from a Python source ﬁle.  *
+# Check each line in the ﬁle to determine if a #character is  *
+# present. If it is then your program should remove all of    *
+# the characters from the #character to the end of the line   *
+# (we’ll ignore the situation where the comment character     *
+# occurs inside of a string). Save the modiﬁed ﬁle using a    *
+# new name that will be entered by the user. The user will    *
+# also enter the name of the input ﬁle. Ensure that an        *
+# appropriate error message is displayed if a problem is      *
+# encountered while accessing the ﬁles.                       *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
+def func_name(user_in):
+  return
+#--------------------------------------------------------------
 if __name__ == "__main__":
-  file_names = input("Enter the file names: ")
-  file_list = []
-  file_list = file_names.split()
-  with open(file_list[0], "r") as file:
-    line_num = 1
-    with open(file_list[1], "w") as file2:
-      for line in file:
-        print(f"{line_num}: {line}", end="")
-        file2.write(f"{line_num}: {line}")
-        line_num += 1
-  print("\n"+"Thank you for using this app.")
+  print("Thank you for using this app.")
 #**************************************************************
