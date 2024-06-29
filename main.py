f0b50e4d49bc693967ece451c48be3dde66e3c9f
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-06-29 14:48:16 +0000

    C0629241300
    - continue coding exercise 153

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 #!/bin/bash
 #**************************************************************
-# Date: 051124 (Expected Solution with 37 Lines of Code)      *
-# Title: Two Word Random Password                             *
+# Date: 051124 (Expected Solution with 59 Lines of Code)      *
+# Title: What’s that Element Again?                           *
 # Status: In Progress (In Progress / Testing / Working)       *
-# While generating a password by selecting random characters  *
-# generally gives a rela- tively secure password, it also     *
-# generally gives a password that is difﬁcult to memorize. As *
-# an alternative, some systems construct a password by taking *
-# two English words and concatenating them. While this        *
-# password isn’t as secure, it is much easier to memorize.    *
-# Write a program that reads a ﬁle containing a list of       *
-# words, randomly selects two of them, and concatenates them  *
-# to produce a new password. When producing the password      *
-# ensure that the total length is between 8 and 10            *
-# characters, and that each word used is at least three       *
-# letters long. Capitalize each word in the password so that  *
-# the user can easily see where one word ends and the next    *
-# one begins. Display the password for the user.              *
+# Write a program that reads a file containing information    *
+# about chemical elements and stores it in one or more        *
+# appropriate data structures. Then your program should read  *
+# and process input from the user. If the user enters an      *
+# integer then your program should display the symbol and     *
+# name of the element with the number of protons entered. If  *
+# the user enters a string then your program should display   *
+# the number of protons for the element with that name or     *
+# symbol. Your program should display an appropriate error    *
+# message if no element exists for the name, symbol or num-   *
+# ber of protons entered. Continue to read input from the     *
+# user until a blank line is entered.                         *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
