d78ef31c044e6f3315d47730bced12689bf69394
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-07-07 09:12:00 +0000

    C0707241730
    - continue coding 605 leet code

--- 00LC0605.py
+++ 00LC0605.py
@@ -1,10 +1,26 @@
 class Solution:
     def cPF(self, fB: list[int], n: int) -> bool:
-        slots = fB.count(0) % n
-        return slots % 2 == 0
+        #slots = fB.count(0) % n
+        #return slots % 2 == 0
+        idx = 0
+        while True:
+            if fB[idx] == 0:
+                if fB[idx+1] == 0:
+                    return True
+            
+
+
+
+                
+    
+            
+            
+        if n == 0:
+            return True
+                
 
 ans_list = Solution()
-list1 = [1, 0, 0, 0, 1]
-num = 1
+list1 = [1, 0, 0, 0, 0, 1]
+num = 2
 
 print(ans_list.cPF(list1, num))
