6e0be42d8d050f9136c9983055957f67b657db38
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-07-19 06:43:31 +0000

    C0719241500
    - completed 152 workbook exercise and started a new leet code exercise.

--- main.py
+++ main.py
@@ -19,18 +19,43 @@
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def read_element_info(user_data):
-  return
-#--------------------------------------------------------------
 if __name__ == "__main__":
+  input_file = "test05.txt"
+  temp_list = []
+  element_dict = {}
+  find_flag = False
+
+  with open(input_file, "r") as chemelem:
+    element_record = chemelem.readlines()
+
+  for idx, readline in enumerate(element_record):
+    temp_list = readline.split(", ")
+    element_dict[int(temp_list[2])] = [temp_list[0], temp_list[1], temp_list[3], temp_list[4][0:-1]]
+
+  while True:
+    user_data = input("Please enter the element number, name, symbol or atomic number: ")
+
+    if user_data == "":
+      print("Thank you for using this app.")
+      break
   
-  user_input = input("Please enter the element's name, symbol or number of proton: ")
-  while user_input != "":
-    if user_input.isdigit():
-      print("The element's name and symbol is: ", read_element_info(user_input))
-    elif user_input.isalpha():
-      print("The element's number of protons is: ", read_element_info(user_input))
-    else:
-      print("Invalid Input!")
-  print("Thank you for using this app.")
+    if user_data.isdigit():
+      if int(user_data) in element_dict:
+        print("Element Number:", user_data)
+        print("Element Name:", element_dict[int(user_data)][0])
+        print("Element Symbol:", element_dict[int(user_data)][1])
+        print("Number of Protons:", user_data)
+      else:
+        print("Element not found!")
+    elif user_data.isalpha():
+      for idx, keys in enumerate(element_dict):
+        if user_data.capitalize() in element_dict[keys]:
+          find_flag = True
+          print("Element Number:", keys)
+          print("Element Name:", element_dict[keys][0])
+          print("Element Symbol:", element_dict[keys][1])
+          print("Number of Protons:", keys)
+          break
+      if idx == len(element_dict) - 1 and find_flag == False:
+        print("Element not found!")
 #**************************************************************
