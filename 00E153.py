f9db2e0df9795b00ccbd1c1b388fd5a243b6c9e0
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-07-21 09:27:11 +0000

    C0721241730
    - started working on exercise 153

--- 00E153.py
+++ 00E153.py
@@ -1,24 +1,23 @@
 #!/bin/bash
-#**************************************************************
-# Date: 070224 (Expected Solution with 49 Lines of Code)      *
-# Title: A Book with No “e” …                                   *
-# Status: In Progress (In Progress / Testing / Working)       *
+#*************************************************************
+# Date: 070224 (Expected Solution with 49 Lines of Code)     *
+# Title: A Book with No “e” …                                *
+# Status: In Progress (In Progress / Testing / Working)      *
 #  The novel “Gadsby” is over 50,000 words in length. While  *
-# 50,000 words isn’t nor- mally remarkable for a novel, it is*
+# 50,000 words isn’t normally remarkable for a novel, it is  *
 # in this case because none of the words in the book use the *
 # letter “e”. This is particularly noteworthy when one       *
 # considers that “e” is the most common letter in English.   *
-# Write a program that reads a list of words from a ﬁle and  *
-# determines what pro- portion of the words use each letter  *
+# Write a program that reads a list of words from a file and *
+# determines what proportion of the words use each letter    *
 # of the alphabet. Display the result for all 26 letters.    *
 # Include an additional message identifying the letter that  *
-# is used in the small- est proportion of the words. Your    *
+# is used in the smallest proportion of the words. Your      *
 # program should ignore any punctuation marks and it should  *
-# treat uppercase and lowercase letters as equivalent. This  *
-# copy belongs to 'acha04'74 7 File and Exception Exercises  *
-#                                                             *
-# Computed Result Validated:                                  *
-#**************************************************************
+# treat uppercase and lowercase letters as equivalent.       *
+#                                                            *
+# Computed Result Validated:                                 *
+#*************************************************************
 #--------------------------------------------------------------
 def func_name(user_in):
   return
