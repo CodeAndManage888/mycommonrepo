94b6c4ac0a44144b4439129f94ed14f51afaa833
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-07-30 05:21:29 +0000

    C07302024
    - generated template for exercise 163 to 165

---
+++ 00E163.py
@@ -0,0 +1,24 @@
+#!/bin/bash
+#**************************************************************
+# Date: 073024 (Expected Solution with 56 Lines of Code)      *
+# Title: Words with Six Vowels in Order                         *
+# Status: In Progress (In Progress / Testing / Working)       *
+#  There is at least one word in the English language that   *
+# contains each of the vowels a,e,i,o,uandyexactly once and  *
+# in order. Write a program that searches a ﬁle containing a *
+# list of words and displays all of the words that meet this *
+# constraint. The user will provide the name of the ﬁle that *
+# will be searched. Display an appropriate error message and *
+# exit the program if the user provides an invalid ﬁle name  *
+# or if something else goes wrong while searching for words  *
+# with six vowels in order.                                  *
+#                                                             *
+# Computed Result Validated:                                  *
+#**************************************************************
+#--------------------------------------------------------------
+def func_name(user_in):
+  return
+#--------------------------------------------------------------
+if __name__ == "__main__":
+  print("Thank you for using this app.")
+#**************************************************************
