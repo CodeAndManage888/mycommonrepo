d50203893c831bb5e21c31c54e9ea6ecc673d2a9
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-08-03 15:19:11 +0000

    C0803242330
    - generate the template for 167 to 170

---
+++ 00E167.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+#**************************************************************
+# Date: 080324 (Expected Solution with 29 Lines of Code)      *
+# Title: Recursive Palindrome                                   *
+# Status: In Progress (In Progress / Testing / Working)       *
+#  The notion of a palindrome was introduced previously in   *
+# Exercise 72. In this exer- cise you will write a recursive *
+# function that determines whether or not a string is a      *
+# palindrome. The empty string is a palindrome, as is any    *
+# string containing only                                     *
+#                                                             *
+# Computed Result Validated:                                  *
+#**************************************************************
+#--------------------------------------------------------------
+def func_name(user_in):
+  return
+#--------------------------------------------------------------
+if __name__ == "__main__":
+  print("Thank you for using this app.")
+#**************************************************************
