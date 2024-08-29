792344c9209b32c8ff9bca6e43a9e4c026fb45fd
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-08-29 15:44:41 +0000

    C0829240000
    - clean up the generated templates for 157 and 158.

--- 00E157.py
+++ 00E157.py
@@ -1,18 +1,18 @@
 #!/bin/bash
 #**************************************************************
 # Date: 072824 (Expected Solution with 41 Lines of Code)      *
-# Title: Distinct Names                                         *
+# Title: Distinct Names                                       *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  In this exercise, you will create a program that reads    *
-# every ﬁle in the baby names data set described in Exercise *
-# 154. As your program reads the ﬁles, it should keep track  *
-# of each name used for a boy and each name used for a girl. *
-# Your program should This copy belongs to 'acha04'Exercise  *
-# 157: Distinct Names 75 output two lists. One list will     *
-# contain all of the names that have been used for girls. The*
-# other list will contain all of the names that have been    *
-# used for boys. Neither of your lists should contain any    *
-# repeated values.                                           *
+#  In this exercise, you will create a program that reads     *
+# every ﬁle in the baby names data set described in Exercise  *
+# 154. As your program reads the ﬁles, it should keep track   *
+# of each name used for a boy and each name used for a girl.  *
+# Your program should This copy belongs to 'acha04'Exercise   *
+# 157: Distinct Names 75 output two lists. One list will      *
+# contain all of the names that have been used for girls. The *
+# other list will contain all of the names that have been     *
+# used for boys. Neither of your lists should contain any     *
+# repeated values.                                            *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
