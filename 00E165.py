ea1d13b5dd7ee38f5ca9be91fdb44446c9e2be15
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-09-05 02:54:57 +0000

    C0905241100
    - clean up template for exercise 165

--- 00E165.py
+++ 00E165.py
@@ -1,17 +1,17 @@
 #!/bin/bash
 #**************************************************************
 # Date: 073024 (Expected Solution with 24 Lines of Code)      *
-# Title: Greatest Common Divisor                                *
+# Title: Greatest Common Divisor                              *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  Euclid was a Greek mathematician who lived approximately  *
-# 2,300 years ago. His algorithm for computing the greatest  *
-# common divisor of two positive integers, aand b, is both   *
-# efﬁcient and recursive. It is outlined below: Ifbis 0then  *
-# Return a Else Setcequal to the remainder when ais divided  *
-# by b Return the greatest common divisor of bandc Write a   *
-# program that implements Euclid’s algorithm and uses it to  *
-# determine the greatest common divisor of two integers      *
-# entered by the user.                                       *
+#  Euclid was a Greek mathematician who lived approximately   *
+# 2,300 years ago. His algorithm for computing the greatest   *
+# common divisor of two positive integers, aand b, is both    *
+# efﬁcient and recursive. It is outlined below: If bis 0 then *
+# Return a Else Setc equal to the remainder when a is divided *
+# by b Return the greatest common divisor of band c Write a   *
+# program that implements Euclid’s algorithm and uses it to   *
+# determine the greatest common divisor of two integers       *
+# entered by the user.                                        *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
