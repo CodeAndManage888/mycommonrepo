8ef794927646b929a0a5080f3bdbfbe15b1c574e
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-09-08 13:51:09 +0000

    C0908242200
    - clean up template for exercise 168

--- 00E168.py
+++ 00E168.py
@@ -1,31 +1,31 @@
 #!/bin/bash
 #**************************************************************
 # Date: 080324 (Expected Solution with 20 Lines of Code)      *
-# Title: Recursive Square Root                                  *
+# Title: Recursive Square Root                                *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  Exercise 71 explored how iteration can be used to compute *
-# the square root of a number. In that exercise a better     *
-# approximation of the square root was generated with each   *
-# additional iteration of a loop. In this exercise you will  *
-# use the same approxi- mation strategy, but you will use    *
-# recursion instead of iteration. Create a square root       *
-# function that takes two parameters. The ﬁrst parameter, n, *
-# will be the number for which the square root is being      *
-# computed. The second parameter, guess , will be the current*
-# guess for the square root. The guess parameter should have *
-# a default value of 1.0. Do not provide a default value for *
-# the ﬁrst parameter. Your square root function will be      *
-# recursive. The base case occurs when guess2is within       *
-# 10−12ofn. In this case your function should return guess   *
-# because it is close enough to the square root of n.        *
-# Otherwise your function should return the result of calling*
-# itself recursively with nas the ﬁrst parameter andguess+n  *
-# guess 2as the second parameter. Write a main program that  *
-# demonstrate your square root function by computing the     *
-# square root of several different values. When you call your*
-# square root function from the main program you should only *
-# pass one parameter to it so that the default value for     *
-# guess is used.                                             *
+#  Exercise 71 explored how iteration can be used to compute  *
+# the square root of a number. In that exercise a better      *
+# approximation of the square root was generated with each    *
+# additional iteration of a loop. In this exercise you will   *
+# use the same approximation strategy, but you will use       *
+# recursion instead of iteration. Create a square root        *
+# function that takes two parameters. The first parameter, n, *
+# will be the number for which the square root is being       *
+# computed. The second parameter, guess , will be the current *
+# guess for the square root. The guess parameter should have  *
+# a default value of 1.0. Do not provide a default value for  *
+# the first parameter. Your square root function will be      *
+# recursive. The base case occurs when guess 2 is within      *
+# 10−12 of n. In this case your function should return guess  *
+# because it is close enough to the square root of n.         *
+# Otherwise your function should return the result of calling *
+# itself recursively with n as the first parameter and guess  *
+# +n guess 2 as the second parameter. Write a main program    *
+# that demonstrate your square root function by computing the *
+# square root of several different values. When you call your *
+# square root function from the main program you should only  *
+# pass one parameter to it so that the default value for      *
+# guess is used.                                              *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
