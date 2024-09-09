61a17f57de1e49c5647659a8846c5579b8d91511
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-09-09 11:20:12 +0000

    C0909241930
    - clean up template for exercise 169

--- 00E169.py
+++ 00E169.py
@@ -1,19 +1,20 @@
 #!/bin/bash
 #**************************************************************
 # Date: 080324 (Expected Solution with 42 Lines of Code)      *
-# Title: String Edit Distance                                   *
+# Title: String Edit Distance                                 *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  The edit distance between two strings is a measure of     *
-# their similarity—the smaller the edit distance, the more   *
-# similar the strings are with regard to the minimum number  *
-# of insert, delete and substitute operations needed to      *
-# transform one string into the other. Consider the strings  *
-# kitten andsitting . The ﬁrst string can be transformed into*
-# the second string with the following operations: Substitute*
-# the kwith ans, substitute the ewith ani, and insert a gat  *
-# the end of the string. This is the smallest number of      *
-# operations that can be performed to transform kitten       *
-# intositting . As a result, the edit distance is 3.         *
+#  The edit distance between two strings is a measure of      *
+# their similarity—the smaller the edit distance, the more    *
+# similar the strings are with regard to the minimum number   *
+# of insert, delete and substitute operations needed to       *
+# transform one string into the other. Consider the strings   *
+# kitten and sitting . The first string can be transformed    *
+# into the second string with the following operations:       *
+# Substitute the k with an s, substitute the e with an i, and *
+# insert a g at the end of the string. This is the smallest   *
+# number of operations that can be performed to               *
+# transform kitten into sitting. As a result, the edit        *
+# distance is 3.                                              *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
