f225a151cd7b4ad62e0f0355c411efb8f8f4f245
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-09-14 15:26:51 +0000

    C0914242330
    - clean up template for exercise 174

--- 00E174.py
+++ 00E174.py
@@ -1,16 +1,16 @@
 #!/bin/bash
 #**************************************************************
 # Date: 082424 (Expected Solution with 36 Lines of Code)      *
-# Title: Run-Length Encoding                                    *
+# Title: Run-Length Encoding                                  *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  Write a recursive function that implements the run-length *
-# compression technique described in Exercise 173. Your      *
-# function will take a list or a string as its only para-    *
-# meter. It should return the run-length compressed list as  *
-# its only result. Include a main program that reads a string*
-# from the user, compresses it, and displays the run-length  *
-# encoded result. Hint: You may want to include a loop inside*
-# the body of your recursive function.                       *
+#  Write a recursive function that implements the run-length  *
+# compression technique described in Exercise 173. Your       *
+# function will take a list or a string as its only para-     *
+# meter. It should return the run-length compressed list as   *
+# its only result. Include a main program that reads a string *
+# from the user, compresses it, and displays the run-length   *
+# encoded result. Hint: You may want to include a loop inside *
+# the body of your recursive function.                        *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
