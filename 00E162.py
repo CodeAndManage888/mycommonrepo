7d63e2a26047a8f8f10575ffe8ae404b4aef7989
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2024-12-10 15:52:32 +0000

    C1210240000
    - continue coding for exercise 162

--- 00E162.py
+++ 00E162.py
@@ -41,28 +41,27 @@ def func_justify(data_input):
   current_line = ""
   with open(data_input, "r") as f:
     file_data = f.readlines()
-    #print(file_data)
+    print("Data File: ", file_data)
 
   for idx1, line in enumerate(file_data):
     if line != "\n":
       words_para += line.split()
     else:
-      for idx2, item in enumerate(words_para):
-        if len(current_line) + len(item) > max_line_len:
-          print(current_line)
-          current_line = ""
-          current_line += item + " "
-        else:
-          current_line += item + " "
-      remaining_items = words_para[idx2:]
-      total_length = sum(len(word + " ") for word in remaining_items)
-      if total_length < max_line_len:
-        current_line = ""
-        current_line = " ".join(remaining_items)
-        print(current_line)
-      #print(words_para)
-      words_para = []
-      #print(words_para)
+      print("Line Data: ", words_para)
+      #for idx2, item in enumerate(words_para):
+      #  if len(current_line) + len(item) > max_line_len:
+      #    print(current_line)
+      #    current_line = ""
+      #    current_line += item + " "
+      #  else:
+      #    current_line += item + " "
+      #remaining_items = words_para[idx2:]
+      #total_length = sum(len(word + " ") for word in remaining_items)
+      #if total_length < max_line_len:
+      #  current_line = ""
+        #current_line = " ".join(remaining_items)
+        #print(current_line)
+      #words_para = []
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
