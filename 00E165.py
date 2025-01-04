2682312be1dfaca024551a405ef4df5af7584273
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-01-04 06:50:31 +0000

    C0104251500
    - continue coding exercise 165

--- 00E165.py
+++ 00E165.py
@@ -8,7 +8,7 @@
 # common divisor of two positive integers, a and b, is both   *
 # efﬁcient and recursive. It is outlined below: If b is 0 then*
 # Return a Else Set c equal to the remainder when a is divided*
-# by b Return the greatest common divisor of band c Write a   *
+# by b Return the greatest common divisor of b and c. Write a *
 # program that implements Euclid’s algorithm and uses it to   *
 # determine the greatest common divisor of two integers       *
 # entered by the user.                                        *
@@ -17,9 +17,15 @@
 #**************************************************************
 #--------------------------------------------------------------
 def GCD(num1, num2):
-  return
+  if num2 == 0:
+    return num1
+  else:
+    return GCD(num2, num1 % num2)
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  
+  user_num1 = int(input("Enter the 1st number: "))
+  user_num2 = int(input("Enter the 2nd number: "))
+  print("The GCD of %d and %d is %d." % (user_num1, user_num2, GCD(user_num1, user_num2)))
+
   print("Thank you for using this app.")
 #**************************************************************
--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 #!/bin/bash
 #**************************************************************
-# Date: 073024 (Expected Solution with 28 Lines of Code)      *
-# Title: Total the Values                                     *
+# Date: 073024 (Expected Solution with 24 Lines of Code)      *
+# Title: Greatest Common Divisor                              *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  Write a program that reads values from the user until a    *
-# blank line is entered. Display the total of all of the      *
-# values entered by the user (or 0.0 if the first value       *
-# entered is a blank line). Complete this task using          *
-# recursion. Your program may not use any loops. Hint: The    *
-# body of your recursive function will need to read one value *
-# from the user, and then determine whether or not to make a  *
-# recursive call. Your function does not need to take any     *
-# parameters, but it will need to return a numeric result.    *
+#  Euclid was a Greek mathematician who lived approximately   *
+# 2,300 years ago. His algorithm for computing the greatest   *
+# common divisor of two positive integers, a and b, is both   *
+# efﬁcient and recursive. It is outlined below: If b is 0 then*
+# Return a Else Set c equal to the remainder when a is divided*
+# by b Return the greatest common divisor of b and c. Write a *
+# program that implements Euclid’s algorithm and uses it to   *
+# determine the greatest common divisor of two integers       *
+# entered by the user.                                        *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def recur():
-  user_value = input("Please enter the data value: ")
-  if user_value == "":
-    return 0.0
+def GCD(num1, num2):
+  if num2 == 0:
+    return num1
   else:
-    return float(user_value) + recur()
+    return GCD(num2, num1 % num2)
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  user_val1 = input("Please enter the 1st number: ")
-  user_val2 = input("Please enter the 2nd number: ")
-  print(recur())
+  user_num1 = int(input("Enter the 1st number: "))
+  user_num2 = int(input("Enter the 2nd number: "))
+  print("The GCD of %d and %d is %d." % (user_num1, user_num2, GCD(user_num1, user_num2)))
+
   print("Thank you for using this app.")
 #**************************************************************
