91700a363697af3f0712c0b04711a16177d70d70
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-01-14 04:52:03 +0000

    C0114251300
    - start coding exercise 167

--- 00E167.py
+++ 00E167.py
@@ -7,7 +7,14 @@
 # Exercise 72. In this exercise you will write a recursive    *
 # function that determines whether or not a string is a       *
 # palindrome. The empty string is a palindrome, as is any     *
-# string containing only                                      *
+# string containing only one character. Any longer string is  *
+# a palindrome if its first and last characters match, and if *
+# the string formed by removing the first and last characters *
+# is also a palindrome.                                       *
+# Write a main program that reads a string and from the user. *
+# Use your recursive function to determine whether or not the *
+# string is a palindrome. Then display an appropriate message *
+# for the user                                                *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
