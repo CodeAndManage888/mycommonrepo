dcc6803acac4553b7d57178ced159fad29291c2f
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-03-28 08:48:21 +0000

    C0328251700
    - continue coding exercise 172

--- main.py
+++ main.py
@@ -13,24 +13,58 @@
 # element sequence that is constructed can not contain any    *
 # duplicates. Write a program that reads the name of an       *
 # element from the user. Your program should use a recursive  *
-# function to ﬁnd the longest sequence of elements that       *
+# function to find the longest sequence of elements that      *
 # begins with the entered element. Then it should display the *
 # sequence. Ensure that your program responds in a reasonable *
 # way if the user does not enter a valid element name. Hint:  *
-# It may take your program up to two minutes to ﬁnd the       *
+# It may take your program up to two minutes to find the      *
 # longest sequence for some elements. As a result, you might  *
 # want to use elements like Molybdenum and Magnesium as       *
 # your first test cases. Each has a longest sequence that is  *
-# only 8 elements long which your program should ﬁnd in a     *
+# only 8 elements long which your program should find in a    *
 # fraction of a second.                                       *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
+element_list = ['Zinc', 'Arsenic', 'Gold', 'Lead', 'Fluorine', 
+                'Chlorine', 'Manganese', 'Bromine', 'Iodine', 
+                'Astatine', 'Tennessine', 'Bismuth', 'Nickel', 
+                'Helium', 'Lithium', 'Beryllium', 'Sodium', 
+                'Magnesium', 'Aluminum', 'Potassium', 'Calcium', 
+                'Scandium', 'Titanium', 'Vanadium', 'Chromium', 
+                'Gallium', 'Germanium', 'Selenium', 'Rubidium', 
+                'Strontium', 'Yttrium', 'Zirconium', 'Niobium', 
+                'Molybdenum', 'Technetium', 'Ruthenium', 'Rhodium', 
+                'Palladium', 'Cadmium', 'Indium', 'Tellurium', 
+                'Cesium', 'Barium', 'Lanthanum', 'Cerium', 
+                'Praseodymium', 'Neodymium', 'Promethium', 'Samarium', 
+                'Europium', 'Gadolinium', 'Terbium', 'Dysprosium', 
+                'Holmium', 'Erbium', 'Thulium', 'Ytterbium', 'Lutetium', 
+                'Hafnium', 'Tantalum', 'Rhenium', 'Osmium', 'Iridium', 
+                'Platinum', 'Thallium', 'Polonium', 'Francium', 'Radium', 
+                'Actinium', 'Thorium', 'Protactinium', 'Uranium', 
+                'Neptunium', 'Plutonium', 'Americium', 'Curium', 
+                'Berkelium', 'Californium', 'Einsteinium', 'Fermium', 
+                'Mendelevium', 'Nobelium', 'Lawrencium', 'Rutherfordium', 
+                'Dubnium', 'Seaborgium', 'Bohrium', 'Hassium', 'Meitnerium', 
+                'Darmstadtium', 'Roentgenium', 'Copernicium', 'Nihonium', 
+                'Flerovium', 'Moscovium', 'Livermorium', 'Hydrogen', 'Boron', 
+                'Carbon', 'Nitrogen', 'Oxygen', 'Neon', 'Silicon', 'Argon', 
+                'Iron', 'Krypton', 'Tin', 'Xenon', 'Tungsten', 'Radon', 
+                'Oganesson', 'Sulfur', 'Copper', 'Silver', 'Phosphorus', 
+                'Cobalt', 'Antimony', 'Mercury']
 #--------------------------------------------------------------
-def func_name(user_in):
+def long_seq(user_in):
+  for i in range(len(element_list)):
+    if user_in == element_list[i]:
+      return element_list[i]
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  
+  element_name = input("Enter an element name: ")
+  if element_name.isalpha() and element_name.capitalize() in element_list:
+    print("The longest sequence of elements that begins with", element_name, "is", long_seq(element_name))
+  else:
+    print("Invalid element name.")
   print("Thank you for using this app.")
 #**************************************************************
