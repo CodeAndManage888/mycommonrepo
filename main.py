316b6ac2c7f071af39a7cd6047ec2b49ec44ebe1
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-04-04 02:50:16 +0000

    C0404251100
    - completed coding exercise 173 and started coding 174

--- main.py
+++ main.py
@@ -1,52 +1,23 @@
 #!/bin/bash
 #**************************************************************
-# Date: 091324 (Expected Solution with 33 Lines of Code)      *
-# Title: Run-Length Decoding                                  *
+# Date: 082424 (Expected Solution with 36 Lines of Code)      *
+# Title: Run-Length Encoding                                  *
 # Status: In Progress (In Progress / Testing / Working)       *
-#  Run-length encoding is a simple data compression technique *
-# that can be effective when repeated values occur at         *
-# adjacent positions within a list. Compression is achieved by*
-# replacing groups of repeated values with one copy of the    *
-# value, followed by the number of times that the value       *
-# should be repeated. For example, the list ["A",             *
-# "A","A","A","A","A","A","A","A","A","A","A","B","B", "B",   *
-# "B", "A", "A", "A", "A", "A", "A", "B"] would be compressed *
-# as["A", 12, "B", 4, "A", 6, "B", 1] . Decompression is      *
-# performed by replicating each value in the list the number  *
-# of times indicated. Write a recursive function that         *
-# decompresses a run-length encoded list. Your recursive      *
-# function will take a run-length compressed list as its only *
-# parameter. It will return the decompressed list as its only *
-# result. Create a main program that displays a run-length    *
-# encoded list and the result of decoding it.                 *
+#  Write a recursive function that implements the run-length  *
+# compression technique described in Exercise 173. Your       *
+# function will take a list or a string as its only para-     *
+# meter. It should return the run-length compressed list as   *
+# its only result. Include a main program that reads a string *
+# from the user, compresses it, and displays the run-length   *
+# encoded result. Hint: You may want to include a loop inside *
+# the body of your recursive function.                        *
 #                                                             *
 # Computed Result Validated:                                  *
 #**************************************************************
-final_list = []
 #--------------------------------------------------------------
-def decode_func(user_list, flist):
-  print("Entering New Loop:", len(user_list))
-  if len(user_list) == 0:
-    print("Exiting Function:", flist)
-    return flist
-  else:
-    for i in range(user_list[1]):
-      flist.append(user_list[0])
-      print(i, "Current Value:", flist)
-    print("Before Reduction:", len(user_list))
-    if len(user_list) != 0:
-      user_list.remove(user_list[0])
-    if len(user_list) == 1:
-      user_list.remove(user_list[0])
-    else:
-      user_list.remove(user_list[1])
-    print("Remaining list is", len(user_list))
-    decode_func(user_list, flist)
+def func_name(user_in):
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
-  encoded_list = ["A", 12, "B", 4, "A", 6, "B", 1]
-  print("The encoded list is", encoded_list)
-  print("The decoded list is", decode_func(encoded_list, final_list))
   print("Thank you for using this app.")
 #**************************************************************
