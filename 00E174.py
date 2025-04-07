0789c89d5ab385cb70ef4f8c70687a3c92744171
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-04-07 02:45:31 +0000

    C0407251100
    - continue coding exercise 174

--- 00E174.py
+++ 00E174.py
@@ -15,12 +15,20 @@
 # Computed Result Validated:                                  *
 #**************************************************************
 #--------------------------------------------------------------
-def func_name(user_in):
+def exp_list(input_lst):
+  if len(input_lst) == 0:
+    return []
+  if isinstance(input_lst[0], int):
+    return [input_lst[1]] * input_lst[0] + exp_list(input_lst[2:])
+  else:
+    return [input_lst[0]] + exp_list(input_lst[1:])  
   return
 #--------------------------------------------------------------
 if __name__ == "__main__":
   user_input_lst = input("Enter a charater list: ")
   print(user_input_lst)
-  in_list = [item.strip() for item in user_input_lst.split(',')]
+  in_list = [int(item.strip()) if item.strip().isdigit() else item.strip() for item in user_input_lst.split(',')]
+  print(in_list)
+  print("The uncompressed list is: ", exp_list(in_list))
   print("Thank you for using this app.")
 #**************************************************************
