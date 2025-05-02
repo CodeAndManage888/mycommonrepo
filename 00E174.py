31f9c018c4a07d1fe495086672b687471398f865
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-05-02 15:11:20 +0000

    C0502252230
    - continue coding exercise 174

--- 00E174.py
+++ 00E174.py
@@ -1,54 +1,53 @@
-#!/bin/bash
-#**************************************************************
-# Date: 082424 (Expected Solution with 36 Lines of Code)      *
-# Title: Run-Length Encoding                                  *
-# Status: In Progress (In Progress / Testing / Working)       *
-#  Write a recursive function that implements the run-length  *
-# compression technique described in Exercise 173. Your       *
-# function will take a list or a string as its only para-     *
-# meter. It should return the run-length compressed list as   *
-# its only result. Include a main program that reads a string *
-# from the user, compresses it, and displays the run-length   *
-# encoded result. Hint: You may want to include a loop inside *
-# the body of your recursive function.                        *
-#                                                             *
-# Computed Result Validated:                                  *
-#**************************************************************
-fin_list = []
-#--------------------------------------------------------------
-def exp_list(input_lst, flist):
-  count = 1
-  print("Start Length:", len(input_lst))
-  if len(input_lst) == 0:
-    return flist
-  elif len(input_lst) == 1:
-    #print("entered 2nd elif")
-    flist.append(input_lst[-1:])
-    flist.append(count)
-    #print("2nd elif Check Value:", flist)
-    #print("2nd elif Check Value:",input_lst[count:])
-    return flist
-  elif input_lst[0] == input_lst[1]:
-    #print("entered last elif")
-    for i in range(len(input_lst) - 1):
-      #print("Entered for loop:", len(input_lst) - 1)
-      if input_lst[i] == input_lst[i+1]:
-        #print("Before Append:", flist, "Current Count:", count)
-        count += 1
-      else:
-        #print("Before Append:", flist)
-        #print("Before Append:",input_lst[count:])
-        flist.append(input_lst[count-1:])
-        flist.append(count)
-        #print("Before Recursion:", flist)
-        #print("Before Recursion:",input_lst[count:])
-        exp_list(input_lst[count-1:], flist)
-    return flist
-#--------------------------------------------------------------
-if __name__ == "__main__":
-  user_input_lst = input("Enter a charater list: ")
-  print(user_input_lst)
-  in_list = [item.strip() for item in user_input_lst.split(',')] 
-  print("User Input:", in_list)
-  print("The compressed list is: ", exp_list(in_list, fin_list))
-  print("Thank you for using this app.")
+  #!/bin/bash
+  #**************************************************************
+  # Date: 082424 (Expected Solution with 36 Lines of Code)      *
+  # Title: Run-Length Encoding                                  *
+  # Status: In Progress (In Progress / Testing / Working)       *
+  #  Write a recursive function that implements the run-length  *
+  # compression technique described in Exercise 173. Your       *
+  # function will take a list or a string as its only para-     *
+  # meter. It should return the run-length compressed list as   *
+  # its only result. Include a main program that reads a string *
+  # from the user, compresses it, and displays the run-length   *
+  # encoded result. Hint: You may want to include a loop inside *
+  # the body of your recursive function.                        *
+  #                                                             *
+  # Computed Result Validated:                                  *
+  #**************************************************************
+  fin_list = []
+  #--------------------------------------------------------------
+  def exp_list(input_lst, flist):
+    count = 1
+    print("Start Length:", len(input_lst))
+    if len(input_lst) == 0:
+      return flist
+    elif len(input_lst) == 1:
+      flist.append(input_lst[-1:])
+      flist.append(count)
+      #print("2nd elif Check Value:", flist)
+      #print("2nd elif Check Value:",input_lst[count:])
+      return flist
+    elif input_lst[0] == input_lst[1]:
+      #print("entered last elif")
+      for i in range(len(input_lst) - 1):
+        #print("Entered for loop:", len(input_lst) - 1)
+        if input_lst[i] == input_lst[i+1]:
+          #print("Before Append:", flist, "Current Count:", count)
+          count += 1
+        else:
+          #print("Before Append:", flist)
+          #print("Before Append:",input_lst[count:])
+          flist.append(input_lst[count-1:])
+          flist.append(count)
+          #print("Before Recursion:", flist)
+          #print("Before Recursion:",input_lst[count:])
+          exp_list(input_lst[count-1:], flist)
+      return flist
+  #--------------------------------------------------------------
+  if __name__ == "__main__":
+    user_input_lst = input("Enter a charater list: ")
+    print(user_input_lst)
+    in_list = [item.strip() for item in user_input_lst.split(',')] 
+    print("User Input:", in_list)
+    print("The compressed list is: ", exp_list(in_list, fin_list))
+    print("Thank you for using this app.")
