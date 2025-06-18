a7d5084f65e811ff029c2bb63becef21f365eceb
Frederick "Erick" Feudo <108861194+CodeAndManage888@users.noreply.github.com>
2025-06-18 00:37:08 +0000

    C0618250830
    - continue coding exercise 174

--- main.py
+++ main.py
@@ -19,7 +19,13 @@ fin_list = []
 def exp_list(input_lst, flist):
   for item in input_lst:
     print("item:", item)
-
+    if item not in flist:
+      flist.append(item)
+    else:
+      flist.append(item)
+      flist.append(flist.count(item))
+      flist.remove(item)
+  return flist
 #--------------------------------------------------------------
 if __name__ == "__main__":
   user_input_lst = input("Enter a charater list: ")
